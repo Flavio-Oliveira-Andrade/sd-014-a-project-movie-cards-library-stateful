32c59ba890416e782b8aed5f49d688054867683c
Filipe <andradefilipe2002@gmail.com>
2021-09-04 13:37:43 -0300

    Requisito 2 adiciona um formulario ao SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,33 @@
 // implement SearchBar component here
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-
     const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange } = this.props
+      onSelectedGenreChange } = this.props;
 
     return (
-      
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="Inclui o texto">
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } />
+        </label>
+      </form>
     );
   }
 }
 
-export default SearchBar
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
