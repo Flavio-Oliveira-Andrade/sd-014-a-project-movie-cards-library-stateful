f054997088c225da861df2763a3af8f5fc58baf6
Lys <lysprestes@gmail.com>
2021-09-03 22:22:23 -0300

    Update AddMovie.jsx
    7th task

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,18 +11,46 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+            name="title"
+          />
+        </label>
       </form>
     );
   }
 }
+
 AddMovie.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
