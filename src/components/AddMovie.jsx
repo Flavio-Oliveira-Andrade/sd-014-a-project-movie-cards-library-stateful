96710e2faea674003ca3903428ed969faf24831c
Pally <francisca.bezerr@gmail.com>
2021-09-03 21:59:56 -0300

    questao 1 ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,2 +1,14 @@
 // implement AddMovie component here
-// SEXTOU
+import React from 'react';
+
+// class AddMovie extends React.Component {
+//   // constuctor(){
+//   //     super();
+//   // }
+//   render() {
+//     const { subtitle, storyline, rating, imagePath, bookmarked, genre, title } = this.props;
+//     return( <form>
+
+//     </form>)
+//   }
+// }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { onSelectedGenreChange, selectedGenre, onBookmarkedChange,
+      bookmarkedOnly, onSearchTextChange, searchText } = this.props;
+    return (<p />);
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}.isRequerid;
+
+export default SearchBar;
