ac6b1bc07f412d69f2d226fac9fa497c9d70466c
Filipe <andradefilipe2002@gmail.com>
2021-09-04 14:30:14 -0300

    Requisito 8 adiciona input Titulo no formulario AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,14 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import { property } from 'lodash';
 
 class AddMovie extends React.Component {
   constructor() {
     super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.createInput = this.createInput.bind(this);
 
     this.state = {
       subtitle: '',
@@ -15,13 +20,41 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  createInput = (inputName, inputType, value, dataTestId) => (
+    <input
+      name={ inputName }
+      type={ inputType }
+      value={ value }
+      data-testid={ dataTestId }
+      onChange={ this.handleChange }
+    />
+  );
+
   render() {
     const { onClick } = this.props;
 
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          { this.createInput('title', 'text', title, 'title-input') }
+        </label>
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onclick: PropTypes.func,
+}.isRequired;
 
 export default AddMovie;
