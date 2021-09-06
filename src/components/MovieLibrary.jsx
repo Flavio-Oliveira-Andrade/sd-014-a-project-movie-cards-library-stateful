a6d5e6594dfdca387a33d27ce475229394b8777d
rodPinheiro <rod_bma@hotmail.com>
2021-09-06 18:48:38 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies
+    }
+  }
   render() {
     return (
       <main>
