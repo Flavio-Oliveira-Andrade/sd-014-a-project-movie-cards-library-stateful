a071a575d02340bde9de0ed0289ac2a656a15da6
Lucas Rimis <lucasrimis@gmail.com>
2021-09-06 17:39:10 -0300

    Push para salvar o pouco que fiz ate agora

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,15 @@
 // implement MovieLibrary component here
+import React from "react";
+import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+import AddMovie from "./AddMovie";
+
+class MovieLibrary extends React.Component {
+    render() {
+        return (
+            <SearchBar />
+        )
+    }
+}
+
+export default MovieLibrary;
