0958ad19e9b10b9d13e682824b91b9a914563514
tiagosathler <sathler@gmail.com>
2021-09-06 17:44:52 -0300

    Requisito 6: concluído

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,50 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { eventListener } = this.props;
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+    return (
+      <div>
+        <p>{ subtitle }</p>
+        <p>{ title }</p>
+        <p>{ imagePath }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+        <p>{ genre }</p>
+        <p>{ eventListener }</p>
+      </div>
+    );
+  }
+}
+
+AddMovie.defaultProps = {
+  eventListener: '',
+};
+
+AddMovie.propTypes = {
+  eventListener: PropTypes.func,
+};
+
+export default AddMovie;
