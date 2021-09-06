efbf6a030ba61fdc909fbbe8aaa4ae1a45da8b8f
rodPinheiro <rod_bma@hotmail.com>
2021-09-06 18:04:56 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <section>
-        <form data-testid="add-movie-form"></form>
+        <form data-testid='add-movie-form'>
+          <label data-testid='title-input-label' htmlFor='title'>
+            Título
+            <input type='text' value={title} name='title' onChange={this.handleChange} data-testid="title-input"/>
+          </label>
+        </form>
       </section>
-    )
+    );
   }
 }
-
