f8df9d16871c04e70d25789771a184994c2ee441
rodPinheiro <rod_bma@hotmail.com>
2021-09-06 18:13:16 -0300

    Requisito 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,7 +11,7 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange({ target }) {
@@ -29,19 +29,53 @@ export default class AddMovie extends Component {
         <form data-testid='add-movie-form'>
           <label data-testid='title-input-label' htmlFor='title'>
             Título
-            <input type='text' value={title} name='title' onChange={this.handleChange} data-testid="title-input"/>
+            <input
+              type='text'
+              value={title}
+              name='title'
+              onChange={this.handleChange}
+              data-testid='title-input'
+            />
           </label>
           <label data-testid='subtitle-input-label' htmlFor='subtitle'>
             Subtítulo
-            <input type='text' value={subtitle} name='subtitle' onChange={this.handleChange} data-testid="subtitle-input"/>
+            <input
+              type='text'
+              value={subtitle}
+              name='subtitle'
+              onChange={this.handleChange}
+              data-testid='subtitle-input'
+            />
           </label>
           <label data-testid='image-input-label' htmlFor='imagePath'>
             Imagem
-            <input type='text' value={imagePath} name='imagePath' onChange={this.handleChange} data-testid="image-input"/>
+            <input
+              type='text'
+              value={imagePath}
+              name='imagePath'
+              onChange={this.handleChange}
+              data-testid='image-input'
+            />
           </label>
           <label data-testid='storyline-input-label' htmlFor='storyline'>
             Sinopse
-            <textarea type='text' value={storyline} name='storyline' onChange={this.handleChange} data-testid="storyline-input"></textarea>
+            <textarea
+              type='text'
+              value={storyline}
+              name='storyline'
+              onChange={this.handleChange}
+              data-testid='storyline-input'
+            ></textarea>
+          </label>
+          <label data-testid='rating-input-label' htmlFor='rating'>
+            Avaliação
+            <input
+              type='number'
+              value={rating}
+              name='rating'
+              onChange={this.handleChange}
+              data-testid='rating-input'
+            />
           </label>
         </form>
       </section>
