8fcf73487ec47c1c776ba8214a3f2760b3b05b95
Lucas Freitas <lucasr.freitas@icloud.com>
2021-09-06 18:31:25 -0300

    Adiciona estado inicial a MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+    const { movies } = this.props; // recebe props;
+    this.state = { searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
     );
   }
 }
