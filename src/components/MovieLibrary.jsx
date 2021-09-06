53ccd33b6a30556b9c045306c0875ff17b0c8f73
rodPinheiro <rod_bma@hotmail.com>
2021-09-06 18:37:20 -0300

    Requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react'
+
+export default class MovieLibrary extends Component {
+  render() {
+    return (
+      <main>
+        
+      </main>
+    )
+  }
+}
