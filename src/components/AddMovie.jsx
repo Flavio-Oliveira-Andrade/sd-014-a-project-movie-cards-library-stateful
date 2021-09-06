d1b603ce370ac10f9d7c55f01a93461d9e821892
Lucas Rimis <lucasrimis@gmail.com>
2021-09-06 20:02:33 -0300

    Ate aqui, estao sendo renderizados os forms e os cards, mas ainda nao criei as props nem as funçoes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,73 @@
 // implement AddMovie component here
+import React from "react";
+
+
+class AddMovie extends React.Component {
+    render() {
+        return (
+            <form data-testid="add-movie-form">
+                <label 
+                htmlFor="inputAddMovieTitle" data-testid="title-input-label">Título
+                    <input 
+                    type="text" 
+                    id="inputAddMovieTitle"
+                    data-testid="title-input"
+                    onChange=""/>
+                </label>
+                <label 
+                htmlFor="inputAddMovieSubtitle" data-testid="subtitle-input-label">Subtítulo
+                    <input 
+                    type="text" 
+                    id="inputAddMovieSubtitle"
+                    data-testid="subtitle-input"
+                    onChange=""/>
+                </label>
+                <label 
+                htmlFor="inputAddMovieImage" data-testid="image-input-label">Imagem
+                    <input 
+                    type="text" 
+                    id="inputAddMovieImage"
+                    data-testid="image-input"
+                    onChange=""/>
+                </label>
+                <label 
+                htmlFor="textareaAddMovie" data-testid="storyline-input-label">Sinopse
+                    <textarea 
+                    id="textareaAddMovie"
+                    data-testid="storyline-input"
+                    onChange=""/>
+                </label>
+                <label 
+                htmlFor="inputAddMovieRating" data-testid="rating-input-label">Avaliação
+                    <input 
+                    type="number" 
+                    id="inputAddMovieRating"
+                    data-testid="rating-input"
+                    onChange=""/>
+                </label>
+                <label 
+                htmlFor="selectSearch"
+                data-testid="genre-input-label">
+                    Gênero
+                    <select 
+                    name="" 
+                    id="selectSearch" 
+                    onChange=''
+                    data-testid="genre-input">
+                    <option 
+                    value="action" data-testid="genre-option">Ação</option>
+                    <option 
+                    value="comedy" data-testid="genre-option">Comédia</option>
+                    <option 
+                    value="thriller" data-testid="genre-option">Suspense</option>
+                    </select>
+                </label>
+                <button 
+                data-testid="send-button"
+                onChange=''>Adicionar filme</button>
+            </form>
+        );
+    }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 import AddMovie from "./AddMovie";
+import movies from "/home/lucas/Documentos/trybe-projetos/Front-end/Movie-cards-stateful/sd-014-a-project-movie-cards-library-stateful/src/data";
 
 class MovieLibrary extends React.Component {
     render() {
         return (
-            <SearchBar />
+            <div>
+                <SearchBar />
+                <MovieList movies={ movies }/>
+                <AddMovie />
+            </div>
         )
     }
 }
