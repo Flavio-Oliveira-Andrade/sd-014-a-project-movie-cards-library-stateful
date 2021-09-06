a0233414ccdb5b9fbaeff7e2b716f97f5d5244d4
MuriloM <muriloommaia@gmail.com>
2021-09-06 17:17:56 -0300

    possivelmente requisito 9 e 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,14 +15,37 @@ class AddMovie extends React.Component {
     };
   }
 
-  render() {
-    const { onClick } = this.props;
-    return (
-      <div>
-        test
-      </div>
-    );
-  }
+handleChange = ({ target }) => {
+  console.log('teste');
+}
+
+render() {
+  const { onClick } = this.props;
+  return (
+    <form data-testid="add-movie-form">
+      <label data-testid="title-input-label" htmlFor="title-input-label">
+        Título
+        <input
+          id="title-input-label"
+          type="text"
+          value={ this.state.title }
+          onChange={ this.handleChange }
+          data-testid="title-input"
+        />
+      </label>
+      <label data-testid="image-input-label" htmlFor="image-input-label">
+        Imagem
+        <input
+          id="image-input-label"
+          type="text"
+          value={ this.state.imagePath }
+          onChange={ this.handleChange }
+          data-testid="image-input"
+        />
+      </label>
+    </form>
+  );
+}
 }
 AddMovie.propTypes = {
   onClick: PropTypes.func.isRequired,
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,18 @@
 // implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <>
+      teste
+        <SearchBar />
+        <AddMovie />
+      </>
+    );
+  }
+}
+
+export default MovieLibrary;
