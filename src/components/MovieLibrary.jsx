b30d3da07789176dbff6e4fa1090a1939a8143e0
Douglas Souza <douglassouzabjj@hotmail.com>
2021-09-06 09:08:41 -0300

    c1 - MovieLibrary - Cria componente MovieLibrary e adiciona PropTypes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,29 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div>
+        { movies }
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieLibrary;
