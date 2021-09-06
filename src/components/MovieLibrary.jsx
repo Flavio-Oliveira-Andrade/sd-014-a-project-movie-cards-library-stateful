c703a6b5924903c7f44438f0a56b6c1e1d8b0818
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-06 16:54:11 -0300

    requsito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,19 @@
 // implement MovieLibrary component here!!
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
   render() {
-    return(
-
-    )
+    const { movies } = this.props;
+    return (
+      <main />
+    );
   }
 }
 
-export default MovieLibrary
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
