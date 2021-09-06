778840295f00b360269cf6032938cc11fa422412
rodPinheiro <rod_bma@hotmail.com>
2021-09-06 17:31:37 -0300

    Requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <section>
         <form data-testid='search-bar-form'>
@@ -15,15 +22,31 @@ export default class SearchBar extends Component {
               value={searchText}
               onChange={onSearchTextChange}
             />
-            <label data-testid="checkbox-input-label" htmlFor="">
-            Mostrar somente favoritos
+            <label data-testid='checkbox-input-label' htmlFor='bookmarked'>
+              Mostrar somente favoritos
               <input
-              data-testid="checkbox-input"
-              type='checkbox'
-              name='bookmarked'
-              checked={bookmarkedOnly}
-              onChange={ onBookmarkedChange}/>
-              </label>
+                data-testid='checkbox-input'
+                type='checkbox'
+                name='bookmarkedOnly'
+                checked={bookmarkedOnly}
+                onChange={onBookmarkedChange}
+              />
+            </label>
+            <label data-testid='select-input-label' htmlFor=''>
+            Filtrar por gênero
+              <select
+                data-testid="select-input"
+                name='selectedGenre'
+                data-testid='select-input'
+                value={selectedGenre}
+                onChange={onSelectedGenreChange}
+              >
+                <option data-testid="select-option" value="">Todos</option>
+                <option data-testid="select-option" value="action">Ação</option>
+                <option data-testid="select-option" value="comedy">Comédia</option>
+                <option data-testid="select-option" value="thriller">Suspense</option>
+              </select>
+            </label>
           </label>
         </form>
       </section>
