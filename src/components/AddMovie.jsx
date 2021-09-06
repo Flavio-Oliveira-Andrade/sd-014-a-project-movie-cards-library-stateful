d6946699abf318a58145e6c2a48c3e0702592250
Michel <ferrazmichel@gmail.com>
2021-09-06 11:39:23 -0300

    Iniciando estrutura do projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    const { searchText } = this.props;
+    return (
+      <form >
+   
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
