f90c9b66ee768323b2f4439511e6c4221e503012
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-06 18:49:36 -0300

    requisito 1 - criando componete SearchBar e passando props pra ele

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import MovieCard from './MovieCard';
+import MovieCard from "./MovieCard";
+import SearchBar from "./SearchBar";
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    const searchText = '';
+    const onSearchTextChange = (() => {});
+    const bookmarkedOnly = false;
+    const onBookmarkedChange = (() => {});
+    const selectedGenre = '';
+    const onSelectedGenreChange = (() => {});
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
       </div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.object,
-  ).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
