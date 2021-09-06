6b14818c9b8240625ae2d49cee29c53e69962228
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-06 19:30:36 -0300

    requisito 6 - criando componente AddMovie e add um state a ele

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    };
+}
+  render() {
+    const { onCLick } = this.props;
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default AddMovie;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-import MovieCard from "./MovieCard";
-import SearchBar from "./SearchBar";
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieList extends React.Component {
   render() {
