cab894fe021f74e8d0a570d02e688febfb267c76
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-06 17:07:42 -0300

    requisito 6 completo e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -18,9 +19,11 @@ class AddMovie extends React.Component {
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form />
+      <form data-testid="add-movie-form" />
     );
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
