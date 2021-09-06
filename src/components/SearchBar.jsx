c2e5fb1277b3fe69de601490e4c6c64a835077e4
Augusto <augusto.raminelli>
2021-09-06 17:01:00 -0300

    criação searchBar requisitos 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,42 @@
-// implement SearchBar component here
+import React from 'react';
+// import MovieList from './MovieList';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      // bookmarkedOnly: null,
+      // selectedGenre: '',
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  render() {
+    const { searchText } = this.state;
+    // const { bookmarkedOnly } = this.state;
+    // const { selectedGenre } = this.state;
+    return (
+      <section className="searchBar">
+        <form id="search-bar-form">
+          <label htmlFor="text-input">
+            Inclui o texto
+            <input
+              type="text"
+              name="text"
+              value={ searchText }
+              id="text-input"
+              onChange={ this.onSearchTextChange }
+            />
+          </label>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default SearchBar;
