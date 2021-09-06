eadaa96b816fd16064369a304aed120de7455010
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-06 19:42:37 -0300

    requisito 8 - renderizando um input para obter o titulo do novo filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -11,13 +13,34 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: '',
     };
-}
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { onCLick } = this.props;
-    return(
+    const { title } = this.state;
+
+    return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            onChange={ this.handleChange }
+            type="text"
+            id="title"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+          />
+        </label>
       </form>
-    )
+    );
   }
 }
 
