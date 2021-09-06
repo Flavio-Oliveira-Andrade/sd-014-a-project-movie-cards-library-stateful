43d97c6d44f294ea2e25b77f024207642d962b26
MuriloM <muriloommaia@gmail.com>
2021-09-06 17:48:00 -0300

    Ate requisito 16

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -16,33 +16,59 @@ class AddMovie extends React.Component {
   }
 
 handleChange = ({ target }) => {
-  console.log('teste');
+  const { id, value } = target;
+  console.log(target['data-testid']);
+  this.setState({
+    [id]: value,
+  });
 }
 
 render() {
   const { onClick } = this.props;
+  const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
   return (
     <form data-testid="add-movie-form">
-      <label data-testid="title-input-label" htmlFor="title-input-label">
+      <label data-testid="title-input-label" htmlFor="title">
         Título
-        <input
-          id="title-input-label"
-          type="text"
-          value={ this.state.title }
-          onChange={ this.handleChange }
-          data-testid="title-input"
-        />
+        <input id="title" type="text" value={ title } onChange={ this.handleChange } data-testid="title-input" />
       </label>
-      <label data-testid="image-input-label" htmlFor="image-input-label">
+      <label data-testid="subtitle-input-label" htmlFor="subtitle">
+        Título
+        <input id="subtitle" type="text" value={ subtitle } onChange={ this.handleChange } data-testid="subtitle-input" />
+      </label>
+      <label data-testid="image-input-label" htmlFor="imagePath">
         Imagem
-        <input
-          id="image-input-label"
-          type="text"
-          value={ this.state.imagePath }
+        <input id="imagePath" type="text" value={ imagePath } onChange={ this.handleChange } data-testid="image-input" />
+      </label>
+      <label data-testid="storyline-input-label" htmlFor="storyline">
+        Sinopse
+        <textarea
+          id="storyline"
+          value={ storyline }
           onChange={ this.handleChange }
-          data-testid="image-input"
+          data-testid="storyline-input"
         />
       </label>
+      <label data-testid="rating-input-label" htmlFor="imagePath">
+        Avaliação
+        <input
+          id="rating"
+          type="number"
+          value={ rating }
+          onChange={ this.handleChange }
+          data-testid="rating-input"
+        />
+      </label>
+      <label data-testid="genre-input-label" htmlFor="genre">
+        Gênero
+        <select data-testid="genre-input" id="genre" value={ genre }>
+          <option data-testid="genre-option" value="">Todos</option>
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+      <button type="submit" data-testid="send-button" onClick={ onClick }>Adicionar filme</button>
     </form>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,31 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    const { movie } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: '',
+      selectedGenre: '',
+      movies: movie,
+    };
+  }
+
   render() {
     return (
       <>
-      teste
         <SearchBar />
         <AddMovie />
       </>
     );
   }
 }
-
+MovieLibrary.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default MovieLibrary;
