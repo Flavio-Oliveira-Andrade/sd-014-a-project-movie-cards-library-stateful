eb5bae8e66fe486388128a1c313e9adb54054331
Heitor SF <heitor.sf5@gmail.com>
2021-09-06 20:36:47 -0300

    fixed 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,9 +14,15 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label value='searchText'>
-          <input type="text" onChange="onSearchTextChange" data-testid="text-input" />
+        <label htmlFor='searchText' data-testid="text-input-label">
+          Inclui o texto: 
+          <input type="text" onChange={ onSearchTextChange } data-testid="text-input" value={ searchText } />          
         </label>
+        <label>
+          Mostrar somente favoritos
+          <input htmlFor="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
+        </label>
+        
       </form>
     );
   }
