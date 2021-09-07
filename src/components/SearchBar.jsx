36d9480d4fcaea4689ce3338a7980b25befa9fca
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-07 18:43:26 -0300

    requisito 1

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return(
+
+    )
+  }
+}
+SearchBar.protoTypes = {
+    searchText: PropTypes.string,
+      onSearchTextChange: PropTypes.func,
+      bookmarkedOnly: PropTypes.bool,
+      onBookmarkedChange: PropTypes.func,
+      selectedGenre: PropTypes.string,
+      onSelectedGenreChange: PropTypes.func
+}.isRequired;
+
+export default SearchBar;
