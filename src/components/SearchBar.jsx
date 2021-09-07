e107a3158016568b3b44f91765dbfd25c5b318af
Williamns <williamnstolb@gmail.com>
2021-09-07 17:02:03 -0300

    Requisito completo: 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -38,6 +38,20 @@ class searchBar extends Component {
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
+          <label htmlFor="selected" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              id="selected"
+              value={ selectedGenre }
+              onChange={ onSelectedGenreChange }
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
