258cb98a2bf4cc4a9178241c4ba985a4745e2300
Semnome1505 <Gabriel_1505@outlook.com>
2021-09-07 19:00:19 -0300

    Diminui um pouco o tamanho das linhas ao criar variaveis com os nomes dos names.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ti = 'title-input';
+const si = 'subtitle-input';
+const ip = 'image-input';
+const sli = 'storyline-input';
+const ri = 'rating-input';
+const gi = 'genre-input';
+const bi = 'send-button';
+const nt = 'title';
+const ns = 'subtitle';
+const ni = 'imagePath';
+const nsl = 'storyline';
+const nr = 'rating';
+
 class AddMovie extends React.Component {
   constructor() {
     super();
@@ -39,40 +52,35 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick: click } = this.props;
-    const { title: t, subtitle: s, imagePath: i, storyline: sl, rating: r, genre: g } = this.state;
-    const ti = 'title-input';
-    const si = 'subtitle-input';
-    const ip = 'image-input';
-    const sli = 'storyline-input';
-    const ri = 'rating-input';
-    const gi = 'genre-input';
-    const bi = 'send-button';
+    const { title: t, subtitle: s, imagePath: i } = this.state;
+    const { storyline: sl, rating: r, genre: g } = this.state;
+    const me = this.aE;
 
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="t" data-testid="title-input-label">
           Título
-          <input type="text" id="t" data-testid={ ti } value={ t } onChange={ this.aE } name="title" />
+          <input type="text" id="t" data-testid={ ti } value={ t } onChange={ me } name={ nt } />
         </label>
         <label htmlFor="s" data-testid="subtitle-input-label">
           Subtítulo
-          <input type="text" id="s" data-testid={ si } value={ s } onChange={ this.aE } name="subtitle" />
+          <input type="text" id="s" data-testid={ si } value={ s } onChange={ me } name={ ns } />
         </label>
         <label htmlFor="i" data-testid="image-input-label">
           Imagem
-          <input type="text" id="i" data-testid={ ip } value={ i } onChange={ this.aE } name="imagePath" />
+          <input type="text" id="i" data-testid={ ip } value={ i } onChange={ me } name={ ni } />
         </label>
         <label htmlFor="ta" data-testid="storyline-input-label">
           Sinopse
-          <textarea name="storyline" id="ta" cols="30" rows="10" value={ sl } data-testid={ sli } onChange={ this.aE } />
+          <textarea name={ nsl } id="ta" value={ sl } data-testid={ sli } onChange={ me } />
         </label>
         <label htmlFor="n" data-testid="rating-input-label">
           Avaliação
-          <input type="number" name="rating" id="n" value={ r } data-testid={ ri } onChange={ this.aE } />
+          <input type="number" name={ nr } id="n" value={ r } data-testid={ ri } onChange={ me } />
         </label>
         <label htmlFor="se" data-testid="genre-input-label">
           Gênero
-          <select name="genre" id="se" data-testid={ gi } value={ g } onChange={ this.aE }>
+          <select name="genre" id="se" data-testid={ gi } value={ g } onChange={ me }>
             <option value="action" data-testid="genre-option">Ação</option>
             <option value="comedy" data-testid="genre-option">Comédia</option>
             <option value="thriller" data-testid="genre-option">Suspense</option>
