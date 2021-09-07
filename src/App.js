76dfe8f070f04dbf29c5c7753aee3705f16e9ee1
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-07 15:57:51 -0300

    terminado refatoraçao, mas nao passa mais nos testes... tentando entender o porque

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
-import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
+import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
