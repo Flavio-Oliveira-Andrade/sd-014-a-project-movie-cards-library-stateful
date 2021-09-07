ef1006fc34953a3afb746ad70dd5fbc60e4fb5f0
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-06 21:51:00 -0300

    requisito 11 - renderizando uma textarea  para obter a sinopse do novo filme

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,7 +25,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { onCLick } = this.props;
-    const { title, subtitle, imagePath } = this.state;
+    const { title, subtitle, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <Input
@@ -46,6 +46,16 @@ class AddMovie extends React.Component {
           handleChange={ this.handleChange }
           titulo={ `${'Imagem'}` }
         />
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            onChange={ this.handleChange }
+            id="storyline"
+            name="storyline"
+            value={ storyline }
+            data-testid="storyline-input"
+          />
+        </label>
       </form>
     );
   }
