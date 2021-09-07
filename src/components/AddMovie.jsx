361ce8d84391407fc9b3cfb2c0192bd14bda1d04
JosueTavaress <josueferreira8.jf@gmail.com>
2021-09-07 15:03:38 -0300

    add requisito 6 <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    };
+
+    this.myFunctionCall = this.myFunctionCall.bind(this);
+  }
+
+  myFunctionCall() {
+
+  }
+
+  render() {
+    return (
+      <form action="">
+
+        <button type="button" onClick={ this.myFunctionCall }>enviar</button>
+
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
