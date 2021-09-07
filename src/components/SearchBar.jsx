6e5b5d4c4b62bfe0d589735c70a2394d3810a0fa
Semnome1505 <Gabriel_1505@outlook.com>
2021-09-07 15:26:06 -0300

    Adiciona o Requisito 3.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const { searchText: s, onSearchTextChange: o, bookmarkedOnly } = this.props;
 
     const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
-      <form data-testid="search-bar-form"></form>
+      <form data-testid="search-bar-form">
+        <label htmlFor="i" data-testid="text-input-label">
+          Inclui o texto:
+          <input type="text" value={ s } onChange={ o } data-testid="text-input" id="i" />
+        </label>
+      </form>
     );
   }
 }
