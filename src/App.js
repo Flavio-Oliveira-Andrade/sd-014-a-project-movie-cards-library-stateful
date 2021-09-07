7743cd687659ce435eef27208afcbb25b29b5f80
WalmirLucena <walmirlucena3@gmail.com>
2021-09-07 15:56:31 -0400

    Adiciona SearchBar recebendo props, e cria input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookMarkedOnly: true,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange() {
+    console.log('searchTextchange');
+  }
+
+  onBookmarkedChange() {
+    console.log('bookmarkedchange');
+  }
+
+  onSelectedGenreChange() {
+    console.log('onselectedgenrechange');
+  }
+
+  render() {
+    const { searchText, bookMarkedOnly, selectedGenre } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          onSearchTextChange={ this.onSearchTextChange }
+          searchText={ searchText }
+          bookMarkedOnly={ bookMarkedOnly }
+          selectedGenre={ selectedGenre }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,26 @@
+/* eslint-disable react/prop-types */
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { onSearchTextChange,
+      searchText,
+      bookMarkedOnly,
+      selectedGenre,
+      onBookmarkedChange,
+      onSelectedGenreChange } = this.props;
+    return (<form data-testid="search-bar-form">
+      <label data-testid="text-input-label">
+        Inclui o texto.
+        <input
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    </form>);
+  }
+}
+export default SearchBar;
