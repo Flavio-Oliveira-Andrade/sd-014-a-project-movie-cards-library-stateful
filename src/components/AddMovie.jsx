ede40b8e993f318f0758f4c6b6a4352f56f43481
Semnome1505 <Gabriel_1505@outlook.com>
2021-09-07 16:55:19 -0300

    Adiciona o Requisito 8.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,19 +14,35 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.aE = this.aE.bind(this);
+  }
+
+  aE(event) {
+    const { value } = event.target;
+    this.setState({
+      title: value,
+    });
   }
 
   render() {
-    const { onClick } = this.props;
+    //  const { onClick } = this.props;
+    const { title: t } = this.state;
+    const ti = 'title-input';
 
     return (
-      <form data-testid="add-movie-form"></form>
+      <form data-testid="add-movie-form">
+        <label htmlFor="t" data-testid="title-input-label">
+          Título
+          <input type="text" id="t" data-testid={ ti } value={ t } onChange={ this.aE } />
+        </label>
+      </form>
     );
   }
 }
 
-AddMovie.propTypes = {
+/*  AddMovie.propTypes = {
   onClick: PropTypes.func.isRequired,
-};
+};  */
 
 export default AddMovie;
