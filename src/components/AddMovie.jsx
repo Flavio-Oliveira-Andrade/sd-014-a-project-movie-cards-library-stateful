01a9efb34e922f07bd5d8cfab91a00cd215661b0
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-07 15:52:21 -0300

    requisitos nao passam mais... tentando consertar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -18,9 +18,8 @@ class AddMovie extends React.Component {
   }
 
   handleChange({ target }) {
-    const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [target.name]: value });
   }
 
   createHandleInput(name, type, value, dataTest) {
@@ -36,13 +35,46 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    // const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          {this.createHandleInput('title', 'text', title, 'title-input')}
+          { this.createHandleInput('title', 'text', title, 'title-input') }
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          { this.createInput('subtitle', 'text', subtitle, 'subtitle-input') }
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          Imagem
+          { this.createInput('imagePath', 'text', imagePath, 'image-input') }
+        </label>
+        <label htmlFor="movie-storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Avaliação
+          { this.createInput('rating', 'number', rating, 'rating-input') }
+        </label>
+        <label htmlFor="movie-genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
