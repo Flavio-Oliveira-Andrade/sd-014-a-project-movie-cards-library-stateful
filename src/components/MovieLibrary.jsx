3503eb70910eaddebdcd87cd039be81acfbcdae0
Semnome1505 <Gabriel_1505@outlook.com>
2021-09-07 14:45:25 -0300

    Adiciona o Estado inicial de MovieLibrary.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,14 +1,33 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
     return (
       <>
-
+        <SearchBar />
       </>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
