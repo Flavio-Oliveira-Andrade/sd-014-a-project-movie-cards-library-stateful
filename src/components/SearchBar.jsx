638922b6a1253e901f1cdd53bb65ad3a2d903a65
Heitor SF <heitor.sf5@gmail.com>
2021-09-06 21:11:26 -0300

    did 5 and fixed selectedGenre. also lint doesnt scream anymore

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,7 +25,7 @@ class SearchBar extends React.Component {
           />
         </label>
 
-        <label htmlFor="searchText" data-testid="checkbox-input-label">
+        <label htmlFor="searchBookmark" data-testid="checkbox-input-label">
           Mostrar somente favoritos?
           <input
             data-testid="checkbox-input"
@@ -33,6 +33,20 @@ class SearchBar extends React.Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
+        </label>
+
+        <label htmlFor="searchGenre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            data-testid="select-input"
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
 
       </form>
@@ -45,7 +59,7 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
-  selecteGenre: PropTypes.string.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
   onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
