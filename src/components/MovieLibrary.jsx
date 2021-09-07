7b72b025a167613b53e5fac44b83274d61f954f6
Filipe Brochier <filipe.brochier@gmail.com>
2021-09-07 18:20:01 -0300

    Requisito 15 e 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 
-/* import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
-*/
-
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       searchText: '',
+       bookmarkedOnly: false,
+       selectedGenre: '',
+       movies: this.props.movies,
+    };
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        {/* <h2> My awesome movie library </h2>
+        <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie /> */}
+        <MovieList movies={ movies } />
+        <AddMovie />
       </div>
     );
   }
