9975004d9bdc3dac5fd1d77ae65b792a0183e0ed
JosueTavaress <josueferreira8.jf@gmail.com>
2021-09-07 11:03:36 -0300

    add requisito 3 Renderize um input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
   render() {
+    const { onSearchTextChange, searchText} = this.props;
+
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="labelTexto" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            data-testid="text-input"
+            type="text"
+            value={ searchText }
+            id="labelTexto"
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
