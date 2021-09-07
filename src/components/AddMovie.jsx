ebf8ae8d870cd9f1798e6b22a6b4b4abaf65bbb0
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-07 15:21:19 -0300

    refatorando pois o lint nao aceita mais de 50 linhas

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,56 +17,32 @@ class AddMovie extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
-    const { name, value } = event.target;
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({ [name]: value });
   }
 
+  createHandleInput(name, type, value, dataTest) {
+    return (
+      <input
+        name={ name }
+        type={ type }
+        value={ value }
+        data-testid={ dataTest }
+        onChange={ this.handleChange }
+      />
+    );
+  }
+
   render() {
-    const { onClick } = this.props;
+    // const { onClick } = this.props;
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input
-            value={ title }
-            type="text"
-            data-testid="title-input"
-            name="title"
-            id="title"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input
-            data-testid="subtitle-input"
-            value={ subtitle }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="imagePath" data-testid="image-input-label">
-          Imagem
-          <input data-testid="image-input" />
-        </label>
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            name="storyline"
-            id="storyline"
-            data-testid="storyline-input"
-            value={ storyline }
-          />
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input
-            name="rating"
-            type="number"
-            value={ rating }
-            data-testid="rating-input"
-          />
+          {this.createHandleInput('title', 'text', title, 'title-input')}
         </label>
       </form>
     );
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,10 +2,13 @@
 // implement SearchBar component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+
     const { movies } = this.props;
 
     this.state = {
