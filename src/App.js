5ab2a106eec0f725f60c75f568dd2040757c8a01
tiagosathler <sathler@gmail.com>
2021-09-07 10:01:06 -0300

    Requisito 15: concluído

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onClick = this.onClick.bind(this);
-  }
-
-  onClick(state) {
-    console.log(`App: ${Object.entries(state)}`);
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <AddMovie
-          onClick={ this.onClick }
-        />
+        <MovieLibrary />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,30 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(state) {
+    console.log(`App: ${Object.entries(state)}`);
+    console.table(movies);
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie
+          onClick={ this.onClick }
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
