1420e8626f879e6cb5abde8a04a58d4f4682dd2d
Matheus Souza <ms.conrado25@hotmail.com>
2021-09-07 18:31:49 -0300

    requisitos 16 ao 18 - configurando o estado inicial do MovieLibray, renderizando SearchBar e o MovieList dentro de MovieLibrary. tive que fazer alguma modificaçoes em outro arquivos para que o codigo funcionasse. Ainda nao esta 100% mas finalizando os outros requisitos irei tentar arrumar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,78 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.arrayMovies = this.arrayMovies.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
+  onSearchTextChange({ target }) {
+    this.setState({
+      searchText: target.value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    this.setState({
+      bookmarkedOnly: target.checked,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    this.setState({
+      selectedGenre: target.value,
+    });
+  }
+
+  arrayMovies(movies) {
+    console.log(movies);
+  }
+
+  render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    // referencia: https://stackoverflow.com/questions/45360519/filter-multiple-values-in-react
+    const movie = movies
+      .filter((array) => {
+        if (bookmarkedOnly) return array.bookmarked;
+        return array;
+      })
+      .filter((array) => {
+        if (selectedGenre) return array.genre === selectedGenre;
+        return array;
+      })
+      .filter(({ title, subtitle, storyline }) => title.toUpperCase()
+        .includes(searchText.toUpperCase())
+      || subtitle.toUpperCase().includes(searchText.toUpperCase())
+      || storyline.toUpperCase().includes(searchText.toUpperCase()));
+
+    this.arrayMovies(movie);
+
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movie } />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import SearchBar from './SearchBar';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    const searchText = '';
-    const onSearchTextChange = (() => {});
-    const bookmarkedOnly = false;
-    const onBookmarkedChange = (() => {});
-    const selectedGenre = '';
-    const onSelectedGenreChange = (() => {});
-
     return (
       <div data-testid="movie-list" className="movie-list">
         {movies.map((movie) => (
           <MovieCard key={ movie.title } movie={ movie } />
         ))}
-        <SearchBar
-          searchText={ searchText }
-          onSearchTextChange={ onSearchTextChange }
-          bookmarkedOnly={ bookmarkedOnly }
-          onBookmarkedChange={ onBookmarkedChange }
-          selectedGenre={ selectedGenre }
-          onSelectedGenreChange={ onSelectedGenreChange }
-        />
       </div>
     );
   }
