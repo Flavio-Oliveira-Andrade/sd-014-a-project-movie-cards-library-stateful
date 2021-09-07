2153990f6503476e6cd5965961eab06c0b49877e
Williamns <williamnstolb@gmail.com>
2021-09-07 16:00:21 -0300

    Requisitos completos: 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import Movielibrary from './components/MovieLibrary';
+import movies from './data';
 import './App.css';
 
 function App() {
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class searchBar extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default searchBar;
