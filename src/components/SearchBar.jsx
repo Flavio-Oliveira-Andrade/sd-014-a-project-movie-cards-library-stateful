a56c942bd87f28f3b4edec79cd8986cd743e5726
Victor Hugo Souza <victoraster2014@gmail.com>
2021-09-08 15:12:15 -0300

    iniciando mais um maravilhoso projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label>
+          Inclui o texto
+          <input type="text" value="searchText" onChange="onSearchTextChange" />
+        </label>
+      </form>
+    );
+  }
+}
+export default SearchBar;
