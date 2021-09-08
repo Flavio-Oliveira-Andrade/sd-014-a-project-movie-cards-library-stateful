3faea99aa1b1a658071f3c3aab0226cd2976f0b1
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-08 17:48:06 -0300

    Requisito 8 - Done!

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,12 +12,29 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
