b6cc760234a214a80b013dd86aa9ded2fd6ef5a6
Filipe <andradefilipe2002@gmail.com>
2021-09-08 17:13:21 -0300

    Refatora alguns requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -71,7 +71,7 @@ class AddMovie extends React.Component {
         </label>
         <label htmlFor="storyline" data-testid="storyline-input-label">
           Sinopse
-          { this.createInput('storyline', 'textarea', storyline, 'storyLine-input') }
+          { this.createInput('storyline', 'textarea', storyline, 'storyline-input') }
         </label>
         <label htmlFor="rating" data-testid="rating-input-label">
           Avaliação
@@ -85,9 +85,9 @@ class AddMovie extends React.Component {
             data-testid="genre-input"
             onChange={ this.handleChange }
           >
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
           </select>
         </label>
         <button
