0d1d0bc687ffea099dfee7422aa1b63127e6c96d
Matheus Alves <malves224@gmail.com>
2021-09-08 16:10:35 -0300

    adiciona class AddMovie e dentro da class chama as funçoes render e constructor

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,22 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() { 
+    super();
+
+    this.state = { 
+      
+    };
+
+  }
+  render() {
+    const { onClick } = this.props;
+    return ( 
+      <form action="">
+
+      </form>
+    );
+  }
+}
