b58ec0524ca3c9d19db2dec7cdbf584401e6808f
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-08 19:03:26 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,32 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+
+        <MovieList movies={ movies } />
+
+      </div>
+
+    );
+  }
+}
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieLibrary;
