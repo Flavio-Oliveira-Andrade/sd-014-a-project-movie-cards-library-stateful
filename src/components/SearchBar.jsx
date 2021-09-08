1608c2ad1b424ee2dec24ac5331e30d4b219913c
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-09-08 16:17:00 -0300

    Requisito 4 passando nos testes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,14 +10,25 @@ class SearchBar extends React.Component {
       selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="a" data-testid="text-input-label">
+        <label htmlFor="search" data-testid="text-input-label">
           Inclui o texto:
           <input
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
-            id="a"
+            id="search"
+          />
+        </label>
+        <label htmlFor="checkbox-filter" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            value={ searchText }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+            id="checkbox-filter"
+            checked={ bookmarkedOnly }
           />
         </label>
       </form>
