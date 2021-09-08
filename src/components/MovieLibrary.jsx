7145c9a1644606d627b7317887987c77c7d6f133
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 18:24:10 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    const { movies } = this.props;
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
   render() {
-    const { movies } = this.props
+    const { movies } = this.props;
     return (
-      
-    )
+      <div>aa</div>
+    );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
 
 export default MovieLibrary;
