00221959893d4f4db1e2c88ab75d0ec98c62916b
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 18:38:30 -0300

    Requisito 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   constructor() {
-    const { movies } = this.props;
     super();
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies,
     };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox' ? target.checked : target.value);
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
-      <div>aa</div>
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ this.handleChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ this.handleChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenre={ this.handleChange }
+      />
     );
   }
 }
