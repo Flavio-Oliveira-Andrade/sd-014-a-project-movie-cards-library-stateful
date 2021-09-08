1c235445bfae4a6fe997736fbe2a1d9f517721e8
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 15:15:49 -0300

    Requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,11 +8,25 @@ function SearchBar() {
     onBookmarkedChange,
     selectedGenre,
     onSelectedGenre,
-  } = this.props
+  } = this.props;
+
   return (
     <form data-testid="search-bar-form">
       <label data-testid="text-input-label">
-      <input type="text" data-testid="text-input" value={ searchText } onChange={ onSearchTextChange }></input>
+        <input
+          type="text"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+      <label data-testid="checkbox-input-label">
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
       </label>
     </form>
   );
