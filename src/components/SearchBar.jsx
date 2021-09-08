7156ec6fa326a99485b443398aba23c92d2379bd
Dogl4 <doougllas@hotmail.com.br>
2021-09-08 16:44:39 -0300

    3° requisito | Cria um label e um input, dentro de form.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,18 +3,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
-
-  const { searchText,
-  onSearchTextChange,
-  bookmarkedOnly,
-  onBookmarkedChange,
-  selectedGenre,
-  onSelectedGenreChange } = this.props;
-
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
       <form data-testid="search-bar=form">
-
+        <label data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
