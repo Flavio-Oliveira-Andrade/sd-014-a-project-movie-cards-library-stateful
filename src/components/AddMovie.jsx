be06b453abd197012f652c860efcbc628a52a1e6
BlyNTC <isaacase@hotmail.com>
2021-09-08 18:14:45 -0300

    requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input">
+          <input type="text" name="title-input" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
