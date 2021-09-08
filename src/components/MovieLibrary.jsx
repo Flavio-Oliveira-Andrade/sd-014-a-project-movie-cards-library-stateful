25ac507511f3ecea7ef05986d9a2d20c0cbd9fff
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-08 13:42:25 -0300

    requisito 15 ok

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -18,26 +18,26 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
       movies,
     };
-    this.this.handleChange = this.handleChange.bind(this);
+    // this.this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange({ target }) {
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [target.name]: value });
-  }
+  // handleChange({ target }) {
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   this.setState({ [target.name]: value });
+  // }
 
   render() {
-    const { searchText, selectedGenre, bookmarkedOnly } = this.state;
+    // const { searchText, selectedGenre, bookmarkedOnly } = this.state;
     return (
       <main>
-        <SearchBar
+        {/* <SearchBar
           searchText={ searchText }
           onSearchTextChange={ this.handleChange }
           bookmarkedOnly={ bookmarkedOnly }
           onBookmarkedChange={ this.handleChange }
           selectedGenre={ selectedGenre }
           onSelectedGenreChange={ this.handleChange }
-        />
+        /> */}
       </main>
     );
   }
