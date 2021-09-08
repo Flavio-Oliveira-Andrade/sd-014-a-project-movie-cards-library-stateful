83818a8d82fd4f5bf641cb94531768e1e6bda61a
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-09-08 16:47:18 -0300

    Requisito 7 passando no teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,27 +4,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            subtitle: "",
-            title: "",
-            imagePath: "",
-            storyline: "",
-            rating: 0,
-            genre: "action",
-        }
-    }
-    render() {
-        const { onClick } = this.props;
-        return (
+  constructor() {
+    super();
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+  }
 
-        );
-    }
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+
+      </form>
+    );
+  }
 }
 
 AddMovie.propTypes = {
-    onclick: PropTypes.func,
+  onclick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
