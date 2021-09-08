40eb718287285201512b30fbfb465dfa7a88f5d9
Gabriel <gabrielferreiradsl@hotmail.com>
2021-09-08 16:56:01 -0300

    Requisito 6,7 e 8 do Projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,45 @@
-// implement AddMovie component her
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleForm = this.handleForm.bind(this);
+  }
+
+  handleForm(event) {
+    const { name, value } = event.target;
+    this.setState(() => {
+      if (name === 'title') return ({ title: value });
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
+    return (
+      <form action="form" data-testid="add-movie-form">
+        <label htmlFor="titulo" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="titulo"
+            id="titulo"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleForm }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
