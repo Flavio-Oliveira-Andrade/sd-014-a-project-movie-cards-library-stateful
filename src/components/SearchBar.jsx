c6d01284daa90575583560e0c72fe36b0ece7a15
Matheus Alves <malves224@gmail.com>
2021-09-08 15:19:36 -0300

    adiciona destructuring para componente searchBar e cria from

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,37 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class SearchBar extends Component {
+	render() {
+		const { 
+			searchText, 
+			onSearchTextChange, 
+			bookmarkedOnly,
+			onBookmarkedChange,
+			selectedGenre,
+			onSelectedGenreChange
+		} = this.props
+
+		return (
+			<form data-testid="search-bar-form" >
+				<label htmlFor="">
+					Inclui o texto: 
+					<input type="text" />
+				</label>
+			</form>
+		)
+	}
+}
+
+SearchBar.propTypes = { 
+	searchText: PropTypes.string,
+	onSearchTextChange: PropTypes.func,
+	bookmarkedOnly: PropTypes.bool,
+	onBookmarkedChange: PropTypes.func,
+	selectedGenre: PropTypes.string,
+	onSelectedGenreChange: PropTypes.func,
+}
+
+export default SearchBar;
