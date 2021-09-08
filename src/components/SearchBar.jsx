aafea0ae2348b9177a82945dd3622b997d005cb8
BlyNTC <isaacase@hotmail.com>
2021-09-08 16:16:00 -0300

    requisito 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -33,6 +33,23 @@ class SearchBar extends React.Component {
             data-testid="checkbox-input"
           />
         </label>
+        <label
+          htmlFor="select-input"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            name="select-input"
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>);
   }
 }
