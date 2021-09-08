a2358c010d5166567e3de0edfe3eb7adecf62186
hugo emannuel <hugoemannuel53@gmail.com>
2021-09-08 13:59:14 -0300

    requiito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (<h1> ola </h1>);
+  }
+}
+
+export default SearchBar;
