3bbb021c11e9238b740a3c384ff417be4620c546
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-08 14:58:32 -0300

    Criando nosso form do addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,10 +14,33 @@ class AddMovie extends Component {
   }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+
     return (
       <form>
-        <label htmlFor="titulo">
-          <input type="text" id="titulo" value={ this.state.title } />
+        <label htmlFor="title">
+          Titulo:
+          <input type="text" id="title" value={ title } />
+        </label>
+        <label htmlFor="subtitle">
+          Subtitulo:
+          <input type="text" id="subtitle" value={ subtitle } />
+        </label>
+        <label htmlFor="imagePath">
+          Imagem do filme:
+          <input type="text" id="imagePath" value={ imagePath } />
+        </label>
+        <label htmlFor="storyline">
+          Sinopse:
+          <textarea type="text" id="storyline" value={ storyline } />
+        </label>
+        <label htmlFor="rating">
+          Avaliação:
+          <input type="number" id="rating" value={ rating } />
+        </label>
+        <label htmlFor="genre">
+          Genero:
+          <input type="text" id="genre" value={ genre } />
         </label>
       </form>
     );
