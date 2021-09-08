cc9f0f361057582a1868c5e1c8cb19d6eb160b8f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-09-08 17:40:03 -0300

    Estruturaçao do requisito 8 finalizada

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,7 +8,7 @@ class AddMovie extends React.Component {
     super();
     this.state = {
       subtitle: "",
-      title: "",
+      title: '',
       imagePath: "",
       storyline: "",
       rating: 0,
@@ -16,11 +16,26 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-
+        <label htmlFor="handle-change-title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+            id="handle-change-title"
+          />
+        </label>
       </form>
     );
   }
