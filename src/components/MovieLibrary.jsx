d0a617139a73a0da6fa68001ec81f5416877fb06
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-08 19:21:52 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  render() {
+  constructor() {
+    super();
+
     const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
+  render() {
+    const { searchText, selectedGenre, bookmarkedOnly, movies } = this.state;
     return (
       <div>
-
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
         <MovieList movies={ movies } />
-
+        <AddMovie />
       </div>
 
     );
