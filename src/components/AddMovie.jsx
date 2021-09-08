cbe916ffb10cf0e688e90b5452ab67c53aa8591f
Matheus Alves <malves224@gmail.com>
2021-09-08 16:58:13 -0300

    adiciona handleChange tornando o input title um componente controlado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,19 +15,43 @@ class AddMovie extends Component {
       genre: 'action',
     };
 
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) { 
+    const { name, value } = target;
+    console.log (value);
+    this.setState({ 
+      [name]: value,
+    })
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
-      <form action="">
-
+      <form data-testid="add-movie-form">
+        <label
+          htmlFor="titulo"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            name="title"
+            value={title}
+            id="titulo"
+            type="text"
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
 }
 
-AddMovie.protoTypes = { 
+AddMovie.propTypes = {
   onClick: PropTypes.func.isRequired,
-}
+};
 
 export default AddMovie;
