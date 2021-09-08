addd9acc6ee291943daf5d9327be7373676cc3b2
Robert Nascimento <robert1a7x@gmail.com>
2021-09-08 15:49:59 -0300

    Requisitos 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,15 +12,37 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
 
     return (
       <form data-testid="add-movie-form">
-        
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            name="title"
+            id="title"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
-    )
+    );
   }
 }
 
