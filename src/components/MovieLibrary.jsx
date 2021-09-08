1881afced45420cf7d196615e6de9230e486f766
Lucas Duarte <lucs.rkt@gmail.com>
2021-09-08 16:05:30 -0300

    feat(components): adicionado um select no component SearchBar. Requisito 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: '',
+      searchText: "",
+      bookmarked: false,
+      selectedGenre: "",
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
   }
 
-  onSearchTextChange() {
+  onSearchTextChange() {}
 
-  }
+  onBookmarkedChange() {}
+
+  onSelectedGenreChange() {}
 
   render() {
-    const { searchText } = this.props;
+    const { searchText, bookmarked, selectedGenre } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar searchText={ searchText } onSearchTextChange={ this.onSearchTextChange } />
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarked={bookmarked}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
         <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    const { props: { searchText, onSearchTextChange } } = this;
-    
+    const {
+      props: {
+        searchText,
+        onSearchTextChange,
+        bookmarkedOnly,
+        onBookmarkedChange,
+        selectedGenre,
+        onSelectedGenreChange,
+      },
+    } = this;
+
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="includes-text" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+          <input
+            type="text"
+            id="includes-text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="show-favorites" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            name=""
+            onChange={onBookmarkedChange}
+            checked={bookmarkedOnly}
+            id="show-favorites"
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="select-gender" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            name=""
+            id="select-genre"
+            onChange={onSelectedGenreChange}
+            value={selectedGenre}
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">
+              Todos
+            </option>
+            <option data-testid="select-option" value="action">
+              Ação
+            </option>
+            <option data-testid="select-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="select-option" value="thriller">
+              Suspense
+            </option>
+          </select>
         </label>
       </form>
     );
