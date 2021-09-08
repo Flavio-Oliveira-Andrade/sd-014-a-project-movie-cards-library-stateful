8d393b198102aa0db52719978ea59b6a0e599473
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-08 13:48:18 -0300

    req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,31 +11,40 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
-    const {
-      subtitle,
-      title,
-      imagePath,
-      storyline,
-      rating,
-      genre,
-    } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+            name="title"
+          />
+        </label>
       </form>
     );
   }
 }
 
-AddMovie.propTypes = {
-  subtitle: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  genre: PropTypes.string.isRequired,
-};
+// AddMovie.propTypes = {
+//   subtitle: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   imagePath: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired,
+//   genre: PropTypes.string.isRequired,
+// };
 
 export default AddMovie;
