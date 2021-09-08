96c13d9f7ed8e1ff4ce886a35d589f9a4f1ea7bc
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-08 15:18:04 -0300

    cumpre requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 // implement SearchBar component here
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
   render() {
@@ -11,7 +11,21 @@ class SearchBar extends Component {
       selectedGenre,
       onSelectedGenreChange,
     } = this.props;
-    return null;
+    return (
+      <form action="" data-testid="search-bar-form">
+        <label htmlFor="input-text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            name=""
+            id="input-text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
   }
 }
 
