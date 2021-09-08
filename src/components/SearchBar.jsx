fcaf70a14ec8d4b2851b8ffe3a841af53045eccd
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-08 15:02:52 -0300

    Renders a select within form in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,25 +13,38 @@ class SearchBar extends Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="searchText">
+        <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto:
           <input
             data-testid="text-input"
-            id="searchText"
+            id="text-input"
             onChange={ onSearchTextChange }
             type="text"
             value={ searchText }
           />
         </label>
-
-        <label data-testid="checkbox-input-label" htmlFor="bookmarked">
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
           Mostrar somente favoritos
           <input
             checked={ bookmarkedOnly }
             data-testid="checkbox-input"
-            id="bookmarked"
+            id="checkbox-input"
             onChange={ onBookmarkedChange }
           />
+        </label>
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            id="select-input"
+            onChange={ onSelectedGenreChange }
+            value={ selectedGenre }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
