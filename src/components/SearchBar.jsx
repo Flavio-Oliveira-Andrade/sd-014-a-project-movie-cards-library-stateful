52cb7160333ae0031936ca0202f857c9a5b5c326
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-08 15:19:50 -0300

    Adiciona prop searchText e seu PropType

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const { searchText } = this.props;
     return (
       <section>
         <form data-testid="search-bar-form">
-          <label htmlFor="search">
-            Pesquisar
-            <input type="text" name="search" id="search" />
+          <label htmlFor="search" data-testid="text-input-label">
+            Inclui o texto:
+            <input type="text" name="search" id="search" value={ searchText } />
           </label>
         </form>
       </section>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
 
