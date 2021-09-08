9e3ce2bd08f0a303095330b0a88c076c448bd48e
Caio César <caiovg32@gmail.com>
2021-09-08 14:23:39 -0300

    cria formulario

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      /*  selectedGenre,
+      onSelectedGenreChange, */
+    } = this.props;
     return (
-      <div>
-        <form data-testid="search-bar-form" />
-      </div>
+      <form data-testid="search-bar-form">
+        <label
+          htmlFor="include-text"
+          data-testid="text-input-label"
+        >
+
+          Inclui o texto:
+
+          <input
+            id="include-text"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+
+        </label>
+
+        <label
+          htmlFor="show-favorites"
+          data-testid="checkbox-input-label"
+        >
+
+          Mostrar somente favoritos
+
+          <input
+            id="show-favorites"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            data-testid="checkbox-input"
+          />
+
+        </label>
+
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  /*  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired, */
+};
 
 export default SearchBar;
