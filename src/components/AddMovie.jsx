fe54a24496d51183c8e036fc2f31e4f50c3f4319
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-08 14:32:18 -0300

    Add exercise 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        ola
+        <label htmlFor="new-movie-title" data-testid="title-input-label">
+          Título
+          <input
+            id="new-movie-title"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+          />
+        </label>
       </form>
     );
   }
