e500919eab3f9b11c3632f923f4d86271ed2514e
Andrea <andrealh08@gmail.com>
2021-09-08 18:39:35 -0300

    Requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  // this.state = {
+  //   subtitle: '1',
+  //   title: '1',
+  //   imagePath: '',
+  //   storyline: '',
+  //   rating: 0,
+  //   genre: 'action',
+  // };
+  // }
+
+  // handleChange = (event) => {
+  //   this.setState()
+  // }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form" />
+    );
+  }
+}
+
+export default AddMovie;
