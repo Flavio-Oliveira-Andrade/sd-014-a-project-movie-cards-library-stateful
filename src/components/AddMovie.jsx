5d21bad3f1afa008a6be1c310608de7bb9cc2062
BlyNTC <isaacase@hotmail.com>
2021-09-08 19:19:55 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,11 +13,36 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleChange = ({ target }) => this.setState({
+    [target.name]: (target.type === 'checkbox' ? target.checked : target.value),
+  });
+
+  createInput(type, name, value, dataTestid) {
+    return (
+      <input
+        type={ type }
+        name={ name }
+        value={ value }
+        onChange={ this.handleChange }
+        data-testid={ dataTestid }
+      />
+    );
+  }
+
   render() {
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="title-input">
-          <input type="text" name="title-input" />
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          { this.createInput('text', 'title', title, 'title-input') }
         </label>
       </form>
     );
