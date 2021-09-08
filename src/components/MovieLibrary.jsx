8446c0722c47db046f385dfa84d6a5c7f1680afe
Williamns <williamnstolb@gmail.com>
2021-09-08 17:41:05 -0300

    E la vamos nos...

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
 
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -12,20 +12,20 @@ class MovieLibrary extends Component {
   // }
 
   render() {
-    const { movies } = this.props;
+    // const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={ movies } />
+        <MovieList />
         <AddMovie />
       </div>
     );
   }
 }
 
-AddMovie.propTypes = {
-  movies: PropTypes.arrayOf().isRequired,
-};
+// AddMovie.propTypes = {
+//   movies: PropTypes.arrayOf().isRequired,
+// };
 
 export default MovieLibrary;
