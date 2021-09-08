e40e87c529b7658e860a433134373c2a0cd8e0d9
Amanda Zotelli <amzotelli@gmail.com>
2021-09-08 19:02:33 -0300

    requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      // searchText, uma string
-      // onSearchTextChange, uma callback
-      // bookmarkedOnly, um boolean
-      // onBookmarkedChange, uma callback
-      // selectedGenre, uma string
-      // onSelectedGenreChange, uma callback
-      <h1>oi</h1>
+      <form data-testid="search-bar-form">
+        {/* <label>
+          Nome:
+    <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Enviar" /> */}
+      </form>
     );
   }
 }
