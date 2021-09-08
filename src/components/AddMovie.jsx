c169593ec07418daa7607630bd6b0901f1844469
Heitor SF <heitor.sf5@gmail.com>
2021-09-08 16:43:56 -0300

    something is wrong

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  };
+
+  render() {
+    const {subtitle, title, imagePath, storyline, rating, genre} = this.state;
+    return(
+      <form>
+        
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+// func because it's got to be a callback OnClick
+export default AddMovie;
