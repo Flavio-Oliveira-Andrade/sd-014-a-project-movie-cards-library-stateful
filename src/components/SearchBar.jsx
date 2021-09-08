66af8a907eae8b3704bd2f7695572e5c2645bb7d
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-08 14:46:07 -0300

    Criacao component SearchBar e skip tests de AddMovie

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (<h1>Search Bar</h1>);
+  }
+}
+
+export default SearchBar;
+
+/**
+ * @author Carlos Barros
+ * @version 1.0.0
+ * @since 08/09/2021
+ */
