ffce3625c69d307958d8a40f89e1574c3b846d9c
Gabriel <gabrielferreiradsl@hotmail.com>
2021-09-08 16:13:17 -0300

    Requisito 2 e 3 do Projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { prototype } from 'stack-utils';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      ''
+      <form name="form" data-testid="search-bar-form">
+        <label htmlFor="form" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            type="text"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        ada
+      </form>
     );
   }
 }
 
 SearchBar.propTypes = {
-  prop: PropTypes.shape({
+  props: PropTypes.shape({
     searchText: PropTypes.string,
     onSearchTextChange: PropTypes.func,
     bookmarkedOnly: PropTypes.bool,
