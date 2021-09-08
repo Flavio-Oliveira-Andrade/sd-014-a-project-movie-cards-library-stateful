26932846acb43c7971c6d3efceec103b86c32456
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-08 15:43:10 -0300

    requisito 4 parcialmente ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,32 +7,36 @@ class SearchBar extends Component {
       searchText: '',
       onSearchTextChange: this.changeSearchText,
       bookmarkedOnly: true,
-      // onBookmarkedChange: outraCallback(),
+      onBookmarkedChange: this.favorito,
       selectedGenre: '',
       // onSelectedGenreChange: maisCallBack(),
-    };
-    this.changeSearchText = this.changeSearchText.bind(this);
-  }
+    }
+    this.changeSearchText = this.changeSearchText.bind(this)
 
-  changeSearchText(event) {
+  }
+  changeSearchText(event){
     this.setState({
-      searchText: event.target.value,
-    });
-    console.log(this.state.searchText);
+      searchText: event.target.value
+    })
   }
-
+  favorito(){
+    console.log('favorito')
+  }
   render() {
-    // const {data-testeid} = this.props
-    const { searchText, onSearchTextChange, bookmarkedOnly, selectedGenre } = this.state;
 
-    return (
-      <form>
+    // const {data-testeid} = this.props
+    const {searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre} = this.state
+
+    return ( 
+      <form data-testid="search-bar-form"> 
         <fieldset>
-          <label htmlFor="texto" data-testid="text-input-label">Inclui o texto:</label>
-          <input type="text" value={ searchText } onChange={ this.changeSearchText } />
+          <label htmlFor="texto" data-testid="text-input-label" data-testid="text-input-label">Inclui o texto:</label>
+          <input type="text" value={searchText} onChange={this.changeSearchText} data-testid="text-input"/>
+          <label htmlFor="checkbox" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+          <input type="checkBox" name="checkbox" id="checkbox" data-testid="checkbox-input" checked={bookmarkedOnly} onChange={this.favorito} />
         </fieldset>
       </form>
-    );
+     );
   }
 }
 
