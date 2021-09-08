69fac2a75b864955a660bdde1d969e4db05ee84d
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-09-08 15:47:06 -0300

    Corrige movieLibrary e adiciona resiquitos 1 e 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
-// implement SearchBar component here
+import React from 'react';
+import Proptypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            id="text-input"
+            data-testeid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: Proptypes.string.isRequired,
+  onSearchTextChange: Proptypes.func.isRequired,
+};
+
+export default SearchBar;
