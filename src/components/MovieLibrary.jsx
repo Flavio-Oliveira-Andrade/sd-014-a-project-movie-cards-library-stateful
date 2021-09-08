a6356f95cac6bc9c1f90fab0991457822af36bf5
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-08 16:01:59 -0300

    Adiciona SearchBar ao MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
+/** Customized imports */
 import MovieList from './MovieList';
+import SearchBar from './SearchBar';
 
 /** Data */
 import movies from '../data';
@@ -7,7 +10,10 @@ import movies from '../data';
 class MovieLibrary extends Component {
   render() {
     return (
-      <MovieList movies={ movies } />
+      <main>
+        <SearchBar />
+        <MovieList movies={ movies } />
+      </main>
     );
   }
 }
