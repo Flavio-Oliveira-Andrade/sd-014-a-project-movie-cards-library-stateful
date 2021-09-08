f9ab3dbbb1286e76669a4efae93f320a1254d9c8
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-09-08 18:29:38 -0300

    Requisito 13 passando nos testes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,7 +12,7 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -85,6 +85,20 @@ class AddMovie extends React.Component {
             onChange={ this.handleChange }
             id="handle-change-rating"
           />
+        </label>
+        <label htmlFor="handle-change-genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            value={ genre }
+            name="genre"
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+            id="handle-change-genre"
+          >
+              <option data-testid="genre-option" value="action" >Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
