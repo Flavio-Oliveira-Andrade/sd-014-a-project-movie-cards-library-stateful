bbb0b02641a920d848aa687e5e57c9b6959087ca
Thiago Oshiro <oshirothiago@gmail.com>
2021-09-08 17:53:17 -0300

    requisito SearchBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,12 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <h1>componente</h1>
+    );
+  }
+}
+
+export default AddMovie;
