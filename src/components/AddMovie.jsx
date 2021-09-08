93e2b0785c079fbc4054e11b031d774cbe36c4f2
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-08 15:53:35 -0300

    Creates AddMovie component

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return <h1>Hello, world!</h1>;
+  }
+}
+
+export default AddMovie;
