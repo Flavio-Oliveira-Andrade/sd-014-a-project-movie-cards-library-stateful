4bf9c3ab77211410a815f66570121bb596bf87cd
Dogl4 <doougllas@hotmail.com.br>
2021-09-08 17:08:06 -0300

    6° Requisito | Cria o component <AddMovie /> e edita o this.state dele.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    // Chamando o pai
+    super(props);
+    // Criando estado e definindo estado inicial
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form />
+    );
+  }
+}
+
+export default AddMovie;
