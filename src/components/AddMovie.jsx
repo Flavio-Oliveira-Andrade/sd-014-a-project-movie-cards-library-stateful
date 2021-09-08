cc1ab30ada72f9c241baa60f22928043a5c2ff2b
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-08 18:58:54 -0300

    Adiciona title e subtitle ao form addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,8 +14,34 @@ class AddMovie extends Component {
   }
 
   render() {
+    const { title, subtitle } = this.state;
     return (
-      <form />
+      <form data-testid="add-movie-form">
+        <label htmlFor="titulo" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="titulo"
+            id="titulo"
+            value={ title }
+            data-testid="title-input"
+            onChange={ handleChangeTitle }
+          />
+        </label>
+
+        <label htmlFor="subtitulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitulo"
+            id="subtitulo"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ handleChangeSubtitle }
+          />
+        </label>
+      </form>
+
     );
   }
 }
