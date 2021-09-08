c4eb8053b4524657f84763e6ffcab383a4f75a66
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-09-08 16:42:31 -0300

    Requisitos 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class AddMovie extends React.Component {
   constructor() {
-    super()
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
     this.state = {
       subtitle: '',
       title: '',
@@ -10,12 +14,52 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
   }
 
+  handleChange({ target }) { // / desestrutura 'event.target' para 'target'
+    const { name } = target; // acessa 'name' de 'value' (pega todos os names, ex 'title')
+    const value = target.type === 'checkbox' ? target.checked : target.value; // para checkbox o bolleano de retorno não fica em 'value', mas sim em 'checkbox'
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // handleClick() {
+  //   const { onClick } = this.props;
+  // }
+
   render() {
-    const { onClick } = this.props;
-    return()
+    return (
+      <form data-testid="add-movie-form">
+
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            name="title"
+            type="text"
+            id="title"
+            data-testid="title-input"
+            value={ this.state.title }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            type="text"
+            id="subtitle"
+            data-testid="subtitle-input"
+            value={ this.state.subtitle }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+      </form>
+    );
   }
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
+
         <label htmlFor="searchText" data-testid="text-input-label">
           Inclui o texto
           <input
