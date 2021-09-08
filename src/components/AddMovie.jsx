5ffbd4190fe0d9545d574f6e7dd3ea59925e1e93
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 16:18:58 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -10,7 +10,15 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
-  }
+    this.handleChange = this.handleChange.bind(this);
+  };
+
+  handleChange({ target }) {
+    const { name, value} = target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
     const {
@@ -19,7 +27,14 @@ class AddMovie extends React.Component {
 
     return (
       <form data-testid="add-movie-form">
-
+        <label data-testid="title-input-label" htmlFor="title">
+          <input
+            value={ this.state.title }
+            data-testid="title-input"
+            name="title"
+            onChange={this.handleChange}
+          />
+        </label>
       </form>
     );
   }
