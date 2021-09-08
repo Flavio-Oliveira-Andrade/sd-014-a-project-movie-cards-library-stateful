67540061ef27d0440251555471e90233e23c7d76
Dogl4 <doougllas@hotmail.com.br>
2021-09-08 15:04:33 -0300

    Iniciando o projeto. :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2>A</h2>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
