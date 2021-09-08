726e2cabf3f48a018776a31314f89a55673f6f79
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 16:28:58 -0300

    Requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,28 +11,46 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
-  };
+  }
 
   handleChange({ target }) {
-    const { name, value} = target;
+    const { name, value } = target;
     this.setState({
       [name]: value,
     });
-  };
+  }
 
   render() {
     const {
       onClick,
     } = this.props;
 
+    const {
+      subtitle,
+      title,
+      imagePath,
+      rating,
+      genre,
+    } = this.state;
+
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title">
+          Título
           <input
-            value={ this.state.title }
+            value={ title }
             data-testid="title-input"
             name="title"
-            onChange={this.handleChange}
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
+          Subtítulo
+          <input 
+            value={ subtitle }
+            data-testid="subtitle-input"
+            name="subtitle"
+            onChange={ this.handleChange }
           />
         </label>
       </form>
