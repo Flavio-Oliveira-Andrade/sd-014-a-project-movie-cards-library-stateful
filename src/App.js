9e806903764bec33d7b0782f589b03abe72521c1
Viviane Freitas <satierfviviane@gmail.com>
2021-09-08 14:29:50 -0300

    Iniciando projeto movie cards stateful

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 
+// comitado para dar o primeiro pr
 function App() {
   return (
     <div className="App">
