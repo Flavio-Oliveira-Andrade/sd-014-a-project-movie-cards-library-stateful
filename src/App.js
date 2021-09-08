47ceab8fe50b246f95c5c0ba3ea9dc1546f3eebe
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-08 14:46:12 -0300

    requisito 6 e 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div>
         <SearchBar />
+        <AddMovie />
       </div>
     </div>
   );
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+          Formulario Add Movie
+        </form>
+      </div>
+
+    );
+  }
+}
+AddMovie.propTypes = { };
+
+export default AddMovie;
