c284d8a6207bf624c6989b0a44629175423ddabe
Filipe <andradefilipe2002@gmail.com>
2021-09-08 14:29:04 -0300

    Requisito 15 cria o componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const [movies] = this.props;
+
+    return movies;
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
+
+export default MovieLibrary;
