94ebf2ac67288e8129ceb78d5d58d67fa44fda3c
Luiz Seidl <luizseidl@gmail.com>
2021-09-08 17:44:20 -0300

    estruturando SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,51 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          <input
+            type="text"
+            id="text-input"
+            value="prop searchText"
+            onChange="prop onSearchTextChange"
+            data-testid="text-input"
+          />
+        </label>
+        <label
+          htmlFor="checkbox-input"
+          data-testid="checkbox-input-label"
+        >
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="checkbox-input"
+            data-testid="checkbox-input"
+            checked="prop bookmarkedOnly"
+            onChange="prop onBookmarkedChange"
+          />
+        </label>
+        <label
+          htmlFor="select-input"
+          data-testid="select-input-label"
+        >
+          Filtrar por gênero
+          <select
+            id="select-input"
+            value="prop selectedGenre"
+            onChange="prop onSelectedGenreChange"
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
