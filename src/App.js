aa818c7110a44ff2597cc525d94622d00f4b3101
Luiz Seidl <luizseidl@gmail.com>
2021-09-08 20:37:55 -0300

    estruturando MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
 
 class App extends React.Component {
@@ -9,8 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <SearchBar />
-        <AddMovie />
+        <MovieLibrary />
       </div>
     );
   }
