f8afab7ec27143f15591bb0fb40620189987df3e
VSSSP <victorssspaula@live.com>
2021-09-08 16:56:01 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <>
-      </>
+      <div>
+        <form data-testid="search-bar-form">
+        </form>
+      </div>
     );
   }
 }
