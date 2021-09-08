76ac0111222a0a1cc737c8a70b778ad1076d4fa3
VSSSP <victorssspaula@live.com>
2021-09-08 17:34:23 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class AddMovie extends Component {
   render() {
+    const { title } = this.props;
     return (
       <form data-testid="add-movie-form">
+        <label data-testid="title-input-label">
+          Título
+          <input
+            value={ title }
+            data-testid="title-input"
+            onChange={ title }
+          />
+        </label>
       </form>
     );
   }
