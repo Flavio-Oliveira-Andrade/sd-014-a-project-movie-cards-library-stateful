fa10b9548baa4d0a711a401f9106b12aecb494c0
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-08 14:45:45 -0300

    Adicionando nosso state com os valores iniciais

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="titulo">
+          <input type="text" id="titulo" value={ this.state.title } />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
