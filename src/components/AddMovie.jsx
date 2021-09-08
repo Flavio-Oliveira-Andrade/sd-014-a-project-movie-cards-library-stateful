43a65bc882cbbe62dde375b35c079480ab1c7450
juli0c3sar <jcmuchiutti@gmail.com>
2021-09-08 18:33:27 -0300

    requisito 6 e 7 completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class AddMovie extends React.Component {
   render() {
     return (
-      <div>AddMovie</div>
+      <form data-testid="add-movie-form">
+        <h1>teste</h1>
+      </form>
     );
   }
 }
