5e84aa3a3f7dd9fed36580ffd0381ebf43a4ef4a
Silas <silas.oliveira8@gmail.com>
2021-09-08 19:31:02 -0300

    requisitos 6 ao 10

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import movies from './data';
+import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <SearchBar onClick={ movies } />
+        <AddMovie />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,23 +2,62 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyLine: '',
-  //     rating: 0,
-  //     genre: 'action',
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value });
+  }
 
   render() {
     // const { onClick } = this.props;
-
+    const { state } = this;
     return (
-      <p>test</p>
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            name="title"
+            onChange={ this.handleChange }
+            value={ state.title }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
+          Subtítulo
+          <input
+            data-testid="subtitle-input"
+            type="text"
+            name="subtitle"
+            onChange={ this.handleChange }
+            value={ state.subtitle }
+          />
+        </label>
+        <label data-testid="image-input-label" htmlFor="imagePath">
+          Imagem
+          <input
+            name="imagePath"
+            type="text"
+            data-testid="image-input"
+            onChange={ this.handleChange }
+            value={ state.imagePath }
+          />
+        </label>
+      </form>
     );
   }
 }
