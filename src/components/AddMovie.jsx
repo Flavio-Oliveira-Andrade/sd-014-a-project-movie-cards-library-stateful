046e3c66dc58043086f66156571dd575151ac533
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-08 17:42:37 -0300

    requisitos 7,8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,14 +13,51 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { title, subtitle } = this.state;
     return (
-      <div>
-        ola
-      </div>
+      <form data-testid="add-movie-form">
+        <label
+          htmlFor="input-titulo"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            type="text"
+            id="input-titulo"
+            data-testid="title-input"
+            value={ title }
+            name="title"
+            onChange={ this.handleOnChange }
+          />
+        </label>
+
+        <label
+          htmlFor="input-subtitulo"
+          data-testid="subtitle-input-label"
+        >
+          Subtítulo
+          <input
+            type="text"
+            id="input-subtitulo"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            name="subtitle"
+            onChange={ this.handleOnChange }
+          />
+        </label>
+      </form>
     );
   }
 }
