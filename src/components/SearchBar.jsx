830c34e010c05dd2a52c56f3a4f4e40860277b0f
Ilan Aragão <ilan_aragao@hotmail.com>
2021-09-08 16:37:19 -0300

    Requisito 1 - Done!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    
+    const {
+      searchtext,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+    <div>
+
+    </div>
+    );
+  }
+}
+
+export default SearchBar;
