3de38090b200c57edd954bf641b24e77a285a07c
Gabriel <gabrielferreiradsl@hotmail.com>
2021-09-08 17:08:41 -0300

    Requisito 9 e 10 do Projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,23 +17,43 @@ class AddMovie extends React.Component {
 
   handleForm(event) {
     const { name, value } = event.target;
-    this.setState(() => {
-      if (name === 'title') return ({ title: value });
-    });
+    this.setState(() => ({ [name]: value }));
   }
 
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form action="form" data-testid="add-movie-form">
         <label htmlFor="titulo" data-testid="title-input-label">
           Título
           <input
             type="text"
-            name="titulo"
-            id="titulo"
+            name="title"
+            id="title"
             data-testid="title-input"
             value={ title }
+            onChange={ this.handleForm }
+          />
+        </label>
+        <label htmlFor="subtitulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            id="subtitle"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={ this.handleForm }
+          />
+        </label>
+        <label htmlFor="imagem" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="imagePath"
+            data-testid="image-input"
+            value={ imagePath }
             onChange={ this.handleForm }
           />
         </label>
