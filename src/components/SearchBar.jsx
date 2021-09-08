4a2ecefedb22e8dd7a4a8784904dd072d8280e42
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-08 11:49:29 -0300

    Adicionando o from

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    // searchText, uma string
+    // onSearchTextChange, uma callback
+    // bookmarkedOnly, um boolean
+    // onBookmarkedChange, uma callback
+    // selectedGenre, uma string
+    // onSelectedGenreChange, uma callback
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return(
+      <form data-testid="search-bar-form">
+       
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
