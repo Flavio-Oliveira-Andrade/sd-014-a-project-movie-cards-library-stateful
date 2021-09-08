0edde32090c235221cade444f86cc62e0ed9d00a
Williamns <williamnstolb@gmail.com>
2021-09-08 14:31:56 -0300

    Primeiro envio apos liberacao

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,13 +1,11 @@
+/* eslint-disable max-lines-per-function */
 // implement AddMovie component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// import LabelInput from './LabelInput';
 
 class AddMovie extends Component {
-  constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-
+  initialState() {
     this.state = {
       subtitle: '',
       title: '',
@@ -18,11 +16,31 @@ class AddMovie extends Component {
     };
   }
 
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+    this.initialState = this.initialState.bind(this);
+
+    this.initialState();
+  }
+
   handleChange(event) {
+    const { name, value } = event.target;
     this.setState({
-      estadoFavorito: event.target.value,
+      [name]: value,
     });
   }
+
+  async addMovie() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    console.log(this.state);
+    this.initialState();
+    console.log(this.state);
+  }
+
   // muitas repetições, trocar futuramente por uma classe única para fazer o trabalho
 
   render() {
@@ -32,43 +50,85 @@ class AddMovie extends Component {
         <form data-testid="add-movie-form">
           <label htmlFor="title-input" data-testid="title-input-label">
             Título
+            <input
+              id="title-input"
+              type="text"
+              value={ title }
+              data-testid="title-input"
+              name="title"
+              onChange={ this.handleChange }
+            />
           </label>
-          <input
-            id="title-input"
-            type="text"
-            value={ title }
-            data-testid="title-input"
-            onChange={ title }
-          />
+
           <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
             Subtítulo
+            <input
+              id="subtitle-input"
+              type="text"
+              value={ subtitle }
+              data-testid="subtitle-input"
+              name="subtitle"
+              onChange={ this.handleChange }
+            />
           </label>
-          <input
-            id="subtitle-input"
-            type="text"
-            value={ subtitle }
-            data-testid="subtitle-input"
-            onChange={ subtitle }
-          />
+
           <label htmlFor="image" data-testid="image-input-label">
             Imagem
+            <input
+              id="image"
+              type="text"
+              value={ imagePath }
+              data-testid="image-input"
+              name="imagePath"
+              onChange={ this.handleChange }
+            />
           </label>
-          <input
-            id="image"
-            type="text"
-            value={ imagePath }
-            data-testid="image-input"
-            onChange={ imagePath }
-          />
+
           <label htmlFor="sinopse" data-testid="storyline-input-label">
             Sinopse
+            <textarea
+              id="sinopse"
+              value={ storyline }
+              data-testid="storyline-input"
+              name="storyline"
+              onChange={ this.handleChange }
+            />
           </label>
-          <textarea
-            id="sinopse"
-            value={ storyline }
-            data-testid="storyline-input"
-            onChange={ storyline }
-          />
+
+          <label htmlFor="rating" data-testid="rating-input-label">
+            Avaliação
+            <input
+              id="rating"
+              type="number"
+              value={ rating }
+              data-testid="rating-input"
+              name="rating"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <label htmlFor="genre" data-testid="genre-input-label">
+            Gênero
+            <select
+              id="genre"
+              value={ genre }
+              onChange={ this.handleChange }
+              data-testid="genre-input"
+            >
+              <option value="action" data-testid="genre-option">
+                Ação
+              </option>
+              <option value="comedy" data-testid="genre-option">
+                Comédia
+              </option>
+              <option value="thriller" data-testid="genre-option">
+                Suspense
+              </option>
+            </select>
+          </label>
+          <button data-testid="send-button" onClick={ this.addMovie }>
+            Adicionar filme
+          </button>
         </form>
       </div>
     );
@@ -82,6 +142,7 @@ AddMovie.propTypes = {
   storyline: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
   genre: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 // implement SearchBar component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
@@ -17,24 +18,24 @@ class searchBar extends Component {
         <form data-testid="search-bar-form">
           <label htmlFor="text-input" data-testid="text-input-label">
             Inclui o texto
+            <input
+              id="text-input"
+              type="text"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
           </label>
-          <input
-            id="text-input"
-            type="text"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-            data-testid="text-input"
-          />
           <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
             Mostrar somente favoritos
+            <input
+              id="checkbox-input"
+              type="checkbox"
+              checked={ bookmarkedOnly }
+              onChange={ onBookmarkedChange }
+              data-testid="checkbox-input"
+            />
           </label>
-          <input
-            id="checkbox-input"
-            type="checkbox"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-            data-testid="checkbox-input"
-          />
           <label htmlFor="selected" data-testid="select-input-label">
             Filtrar por gênero
             <select
@@ -43,10 +44,18 @@ class searchBar extends Component {
               onChange={ onSelectedGenreChange }
               data-testid="select-input"
             >
-              <option value="" data-testid="select-option">Todos</option>
-              <option value="action" data-testid="select-option">Ação</option>
-              <option value="comedy" data-testid="select-option">Comédia</option>
-              <option value="thriller" data-testid="select-option">Suspense</option>
+              <option value="" data-testid="select-option">
+                Todos
+              </option>
+              <option value="action" data-testid="select-option">
+                Ação
+              </option>
+              <option value="comedy" data-testid="select-option">
+                Comédia
+              </option>
+              <option value="thriller" data-testid="select-option">
+                Suspense
+              </option>
             </select>
           </label>
         </form>
