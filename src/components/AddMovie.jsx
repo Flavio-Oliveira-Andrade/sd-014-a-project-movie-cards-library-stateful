15cbb2ff80aaea92317a1fea28e649f073f2859c
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 15:58:10 -0300

    Requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,26 @@
-// import React from 'react';
+import React from 'react';
 
-// class AddMovie extends React.Component {
-//   render() {
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
 
-//   }
-// }
+  render() {
+    const {
+      onClick,
+    } = this.props;
+
+    return (
+      <form>
+
+      </form>
+    );
+  }
+}
