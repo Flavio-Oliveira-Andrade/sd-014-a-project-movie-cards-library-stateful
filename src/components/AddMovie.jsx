4f8287ec5c94efd153d062375f153cf2fd9268b5
hector <hectoreddbenitez@gmail.com>
2021-09-08 16:17:48 -0300

    Adiciona template basico nos componentes AddMovie e SearchBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        oi
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
+
