966beaaa6b3911e8c7f181b1337eb08624e78e80
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-09-08 17:45:46 -0300

    Componentiza o title-input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import movies from '../data';
+import TitleInput from './TitleInput';
+
 class AddMovie extends React.Component {
   constructor() {
     super();
@@ -12,7 +14,7 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.addMovie = this.addMovie.bind(this)
+    this.addMovie = this.addMovie.bind(this);
   }
 
   handleChange({ target }) {
@@ -23,15 +25,16 @@ class AddMovie extends React.Component {
   }
 
   addMovie() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     movies.push({
-      title: this.state.title,
-      subtitle: this.state.subtitle,
-      storyline: this.state.storyline,
-      rating: this.state.rating,
-      imagePath: this.state.imagePath,
+      title,
+      subtitle,
+      storyline,
+      rating,
+      imagePath,
       bookmarked: false,
-      genre: this.state.genre,
-    })
+      genre,
+    });
     this.setState({
       subtitle: '',
       title: '',
@@ -39,24 +42,15 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    })
+    });
   }
 
   render() {
     const { onClick } = this.props;
-    const { subtitle, title, imagePath, storyline, rating} = this.state;
+    const { subtitle, title, imagePath, storyline, rating } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title">
-          Título
-          <input
-            type="text"
-            value={ title }
-            data-testid="title-input"
-            name="title"
-            onChange={ this.handleChange }
-          />
-        </label>
+        <TitleInput value={ title } onChange={ this.handleChange } />
         <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
           <input
@@ -108,7 +102,13 @@ class AddMovie extends React.Component {
             <option data-testid="genre-option" value="thriller">Suspense</option>
           </select>
         </label>
-        <button data-testid="send-button" onClick={ this.addMovie }>Adicionar filme</button>
+        <button
+          data-testid="send-button"
+          onClick={ this.addMovie }
+          type="button"
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
---
+++ src/components/TitleInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class TitleInput extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label data-testid="title-input-label" htmlFor="title">
+        Título
+        <input
+          type="text"
+          value={ value }
+          data-testid="title-input"
+          name="title"
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+TitleInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default TitleInput;
