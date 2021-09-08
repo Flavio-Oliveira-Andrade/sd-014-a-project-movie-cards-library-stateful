16e630bb4ec137d6d271f36a0b81bf9549fea897
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-08 16:04:48 -0300

    Renders text input within form in AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   constructor() {
     super();
+
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -13,10 +15,31 @@ class AddMovie extends Component {
     };
   }
 
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
-    return <form data-testid="add-movie-form" />;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            data-testid="title-input"
+            id="title-input"
+            name="title"
+            onChange={ this.handleChange }
+            type="text"
+            value={ title }
+          />
+        </label>
+      </form>
+    );
   }
 }
 
