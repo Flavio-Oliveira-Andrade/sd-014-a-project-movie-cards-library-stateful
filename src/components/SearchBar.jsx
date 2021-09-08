0a40bff81cddd1a69e54e06cbbec9d31c75c2499
Amanda Zotelli <amzotelli@gmail.com>
2021-09-08 18:35:01 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      // searchText, uma string
+      // onSearchTextChange, uma callback
+      // bookmarkedOnly, um boolean
+      // onBookmarkedChange, uma callback
+      // selectedGenre, uma string
+      // onSelectedGenreChange, uma callback
+      <h1>oi</h1>
+    );
+  }
+}
+
+export default SearchBar;
