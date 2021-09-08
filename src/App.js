e39d1068f3039fef587f9a4c850462d11a71cd5e
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-08 14:53:02 -0300

    cumpre requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+// import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <MovieList /> */}
     </div>
   );
 }
