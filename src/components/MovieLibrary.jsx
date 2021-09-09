411bc478acab1a473a2fc32ae7da2f8d9f9a1d3b
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-09 16:23:15 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape.isRequired,
+};
+
+export default MovieLibrary;
