9c0ae8229d02e7cd5094817ff2411994945ee173
Heitor SF <heitor.sf5@gmail.com>
2021-09-09 00:00:58 -0300

    16 is properly done now. Still figuring out 17.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+// import SearchBar from './components/SearchBar';
+// import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 import './App.css';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-
     const { movies } = this.props;
 
     this.state = {
@@ -14,12 +14,36 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
       movies,
     };
+    this.genericHandler = this.genericHandler.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
   }
 
+  genericHandler({ target }) {
+    const { name } = target;
+    const value = target.type === 'checked' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
+  // onSearchTextChange({ target }) {
+  //   const { name } = target;
+  //   const value = target.type === 'checked' ? target.checked : target.value;
+  //   this.setState({ [name]: value });
+  // }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <section>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+          onChange={ this.genericHandler }
+        />
+        <MovieList movies={ movies } />
       </section>
     );
   }
