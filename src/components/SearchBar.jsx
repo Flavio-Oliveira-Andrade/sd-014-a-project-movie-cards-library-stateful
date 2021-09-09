bf913dd1caee2f5a205c45ec9cbd09f64cc8d850
vixemari <marianarocha308@gmail.com>
2021-09-09 15:25:01 -0300

    Crie um componente chamado <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <main>
+        <input type="text" />
+      </main>
+    );
+  }
+}
+
+export default SearchBar;
