0cd42e7a7464c1631d3a963d1d318537a11094c1
Pedro Barreto <pbdeoliveira@gmail.com>
2021-09-09 17:18:51 -0300

    requisito renderizar SearchText

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,45 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  textInput() {
+    const { onSearchTextChange, searchText } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="searchText">
+        Inclui o texto:
+        <input
+          name={ searchText }
+          onChange={ onSearchTextChange }
+          value={ searchText }
+          type="text"
+          id="searchText"
+          data-testid="text-input"
+        />
+
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          { this.textInput() }
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.string,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+  onSearchTextChange: PropTypes.string,
+};
+
+export default SearchBar;
