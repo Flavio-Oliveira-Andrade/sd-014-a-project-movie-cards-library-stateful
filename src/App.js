4fcb441a1da844288366a92e985f653068098db6
Dogl4 <doougllas@hotmail.com.br>
2021-09-08 21:15:48 -0300

    15° Requisito | Adiciona um props, de entrada para o parametro <MovieLibrary />

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from './components/Header';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieLibrary movies={ data } />
       <AddMovie />
     </div>
   );
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,11 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
   render() {
+    console.log(this.props, 'Xablau');
+    const { movies } = this.props;
     return (
       <div>
         <h2>A</h2>
@@ -10,5 +13,9 @@ class MovieLibrary extends Component {
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
 
 export default MovieLibrary;
