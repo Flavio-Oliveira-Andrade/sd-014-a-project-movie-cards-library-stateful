511ebc3a69f135defe3d6ade020b1c909d53a196
Robert Nascimento <robert1a7x@gmail.com>
2021-09-08 21:07:34 -0300

    Requisitos 19 e 20

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class MovieLibrary extends React.Component {
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   onSearchTextChange({ target }) {
@@ -62,6 +64,12 @@ class MovieLibrary extends React.Component {
     });
   }
 
+  onClick(newMovie) {
+    let { movies } = this.props;
+    movies = [...movies, newMovie];
+    this.setState({ movies });
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
@@ -75,6 +83,7 @@ class MovieLibrary extends React.Component {
           onSelectedGenreChange={ this.onSelectedGenreChange }
         />
         <MovieList movies={ movies } />
+        <AddMovie onClick={ this.onClick } />
       </>
     );
   }
