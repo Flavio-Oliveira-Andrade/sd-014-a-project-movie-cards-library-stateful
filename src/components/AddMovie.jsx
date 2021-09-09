c047c8637bd82f691a67611b3346efdb81fea9f1
hector <hectoreddbenitez@gmail.com>
2021-09-08 22:26:29 -0300

    Adiciona requisito 7 e 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <div>
         <form data-testid="add-movie-form">
-          test
+          <label htmlFor="input" data-testid="title-input-label">
+            Título
+            <input
+              id="input"
+              data-testid="title-input"
+              type="text"
+              onChange={ this.handleChange }
+              value={ title }
+            />
+          </label>
         </form>
       </div>
     );
   }
 }
+
+// AddMovie.propTypes = {
+//   title: PropTypes.string.isRequired,
+// };
 
 export default AddMovie;
