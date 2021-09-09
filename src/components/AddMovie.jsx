5bbd4628b00bfe87aca2f6b64e22655b95c21dee
Amanda Zotelli <amzotelli@gmail.com>
2021-09-09 15:47:09 -0300

    adicionando states em AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,61 +1,90 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  // subtitle: '';
-  // title: '';
-  // imagePath: '';
-  // storyline: '';
-  // rating: 0;
-  // genre: 'action'
+  construtor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  render() {
-    <AddMovie onClick={ () => {} } />;
+  handleSubmit = (event) => {
+    event.preventDefault();
+  //   // const { decidindo } = this.state;
+  //   // enviar p lista
+  // }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+    render(); {
+    const { subtitle, title, imagePath, storyline, rating }
     return (
-      <form data-testid='add-movie-form'>
-        <label htmlFor='titleLabel' data-testid='title-input-label'>
+      <form data-testid="add-movie-form">
+        <label htmlFor="titleLabel" data-testid="title-input-label">
           Título:
           <input
-            type='text'
+            type="text"
             value={ title }
-            onChange={ this.state.title }
-            data-testid='title-input'
+            onChange={ this.handleChange }
+            data-testid="title-input"
           />
         </label>
-        <label htmlFor='subtitleLabel' data-testid='subtitle-input-label'>
+        <label htmlFor="subtitleLabel" data-testid="subtitle-input-label">
           Subtítulo:
           <input
-            type='text'
+            type="text"
             value={ subtitle }
-            onChange={ this.state.subtitle }
-            data-testid='subtitle-input'
+            onChange={ this.handleChange }
+            data-testid="subtitle-input"
           />
         </label>
-        <label htmlFor='imgLabel' data-testid='image-input-label'>
+        <label htmlFor="imgLabel" data-testid="image-input-label">
           Imagem
           <input
-            type='text'
+            type="text"
             value={ imagePath }
-            onChange={ this.state.imagePath }
-            data-testid='image-input'
+            onChange={ this.handleChange }
+            data-testid="image-input"
           />
         </label>
-        <label htmlFor='storylineLabel' data-testid='storyline-input-label'>
+        <label htmlFor="storylineLabel" data-testid="storyline-input-label">
           Sinopse
           <textarea
-            data-testid='storyline-input'
-            value={ this.state.storyline }
-            onChange={ this.state }
+            data-testid="storyline-input"
+            value={ storyline }
+            onChange={ this.handleChange }
           />
         </label>
-        <label htmlFor='ratingLabel' data-testid='rating-input-label'>
+        <label htmlFor="ratingLabel" data-testid="rating-input-label">
           Avaliação
           <input
-            type='number'
-            value={ this.state.rating }
-            onChange={ this.state }
-            data-testid='rating-input'
+            type="number"
+            value={ rating }
+            onChange={ this.handleChange }
+            data-testid="rating-input"
           />
         </label>
+        <label htmlFor="genreLabel" data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            value={ genre }
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button type="submit" data-testid="send-button" onClick={ console.log('oi') }>Adicionar Filme</button>
       </form>
     );
   }
