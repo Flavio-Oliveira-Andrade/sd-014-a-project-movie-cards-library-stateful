fc87b0c03fbe4bc31e28ebd99b67d5c5d97ee67e
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-09 20:08:05 -0300

    Create AddMovie component and add inicial state and also onClick props.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const inicialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: '',
+  genre: '',
+};
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = inicialState;
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <div />
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
