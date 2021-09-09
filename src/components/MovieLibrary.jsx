71fec6066ca07319e6a541b489b63d2733d294b7
Filipe Brochier <filipe.brochier@gmail.com>
2021-09-09 18:40:10 -0300

    Requisito 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
@@ -17,6 +17,13 @@ class MovieLibrary extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.filterMovies = this.filterMovies.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(movie) { // Função para adicionar um novo filme à lista!
+    this.setState(({ movies }) => ({ // Atualiza o estado do componente (e desestrutura a chave "movies")
+      movies: [...movies, movie], // Adiciona as informações do novo filme à lista de filmes já existente (atenção para o spread operator!!!)
+    }));
   }
 
   handleChange({ target }) {
@@ -31,9 +38,9 @@ class MovieLibrary extends Component {
       || movie.subtitle.includes(searchText)
       || movie.storyline.includes(searchText),
     );
-    if (bookmarkedOnly) {
+    if (bookmarkedOnly === true) {
       return filteredMovies.filter((movie) => movie.bookmarked);
-    } if (selectedGenre) {
+    } if (selectedGenre !== '') {
       return filteredMovies.filter((movie) => movie.genre === selectedGenre);
     }
     return filteredMovies;
@@ -52,6 +59,7 @@ class MovieLibrary extends Component {
           onSelectedGenreChange={ this.handleChange }
         />
         <MovieList movies={ this.filterMovies(this.state) } />
+        <AddMovie onClick={ this.handleClick } />
       </main>
     );
   }
