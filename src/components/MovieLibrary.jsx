ceca45421bdbf84d0f6d2bb666cf8537060d6b92
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-09 18:52:56 -0300

    cumpre requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,17 @@
 import React from 'react';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+    const { movies } = props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
     return null;
   }
