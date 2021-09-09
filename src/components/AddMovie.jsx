547839bacfdac1d82a99d2c9fd31ee791891b56a
Amanda Zotelli <amzotelli@gmail.com>
2021-09-09 12:51:13 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-
 // subtitle: '';
-// title: '';
-// imagePath: '';
-// storyline: '';
-// rating: 0;
-// genre: 'action'
-  
+  // title: '';
+  // imagePath: '';
+  // storyline: '';
+  // rating: 0;
+  // genre: 'action'
+
   render() {
-    <AddMovie onClick={ () => { } } />
+    <AddMovie onClick={ () => { } } />;
     return (
-<form data-testid="add-movie-form">
-        
-</form>
+      <form data-testid="add-movie-form">
+        <label htmlFor="titleLabel" data-testid="title-input-label">
+          Título:
+          <input
+            type="text"
+            value={ title }
+            onChange={ this.state.title }
+            data-testid="title-input"
+          />
+        </label>
+      </form>
     );
   }
 }
