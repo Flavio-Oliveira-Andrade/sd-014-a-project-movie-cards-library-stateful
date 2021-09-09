d1ed816269c3316d24a5d9982a5f9601e81238c4
César Meira <cesarmeirasouza@gmail.com>
2021-09-09 17:11:03 -0300

    Req 6,7 e 8...not rdy

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,36 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { title } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            id="title"
+            value={ title }
+            data-testid="title-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
