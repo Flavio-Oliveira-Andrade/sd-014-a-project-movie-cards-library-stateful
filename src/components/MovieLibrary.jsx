09db5834fa39e31b98a778b80d8e2ad24be2a402
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-09 14:46:37 -0300

    criando a parte de filtro do filme por pesquisa

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super();
-    const { movie } = this.props;
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
 
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: movie,
+      movies: movies,
     };
   }
 
@@ -24,20 +25,34 @@ class MovieLibrary extends React.Component {
     });
   }
 
+  handleFilter() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return movies.filter((movie) => movie.title.toLowerCase().icludes(searchText)
+    || movie.subtitle.toLowerCase().includes(searchText)
+    || movie.storyLine.toLowerCase().includes(searchText))
+      .filter((movie) => (selectedGenre ? movie.genre === selectedGenre : movie))
+      .filter((movie) => (bookmarkedOnly ? movie.bookmarked : movie));
+  }
+
   render() {
+    // Desestruturando o state
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
     return (
       <main>
         <SearchBar
           searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
         />
-        {/* <AddMovie />  Criando uma função para passar como paramentro de adiciona os filmes */}
+        <AddMovie movies={ this.handleFilter } />
       </main>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieLibrary;
