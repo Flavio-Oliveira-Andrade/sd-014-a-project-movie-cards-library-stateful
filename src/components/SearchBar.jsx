4e78000c2145432a1e75b3e9bafe6db12c94b33b
Maurício Leite <mauricioleite1@gmail.com>
2021-09-09 11:58:21 -0300

    Requisitos 01 e 02

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import './style/components.css';
+
+export default class SearchBar extends Component {
+  render() {
+    const { id } = this;
+    return (
+      <form data-testid="search-bar-form">
+        <input type="text" value="" placeholder="Buscar" />
+        <label htmlFor={ id }>
+          <input id={ id } type="checkbox" value="" />
+          Mostrar favoritos
+        </label>
+      </form>
+    );
+  }
+}
