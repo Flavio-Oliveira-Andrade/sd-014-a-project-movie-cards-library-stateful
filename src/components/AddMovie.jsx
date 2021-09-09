f64ef31b3b18913c1604758b6bd60ebf24748b9f
Viviane Freitas <satierfviviane@gmail.com>
2021-09-09 15:14:53 -0300

    Req 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,22 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props; // callback
+    return ('');
+  }
+}
+
+export default AddMovie;
