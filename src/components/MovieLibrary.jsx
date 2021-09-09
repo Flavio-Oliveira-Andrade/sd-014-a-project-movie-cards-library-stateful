cca06714b4d7625ff21b0d85255c1142f3572214
Matheus Alves <malves224@gmail.com>
2021-09-08 22:08:29 -0300

    adiciona state inicial para componente MovieLibrary e renderiza componente SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,27 @@
 // implement MovieLibrary component here
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
-
+	constructor() { 
+		const { movies } = this.props
+		super();
+		this.state = { 
+			searchText: '',
+			bookmarkedOnly: false,
+			selectedGenre: '',
+			movies: movies,
+		}
+	}
+	render() { 
+		const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+		return ( 
+			<SearchBar 
+			searchText={ searchText }
+			
+			/>
+		)
+	}
 }
 
 export default MovieLibrary;
