7c3ab908fe14ec65b6c53c0f3db2775c37aaab74
Jonathan Ferreira <j0n4t@hotmail.com>
2021-09-08 23:53:43 -0300

    Requisito 6 ✔

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      // title: '',
+      // story: '',
+      // imagePath: '',
+      // rating: 0,
+      // genre: '',
+    };
+  }
+
   render() {
+    // const { onClick } = this.props;
     return (
       <div>AddMovie</div>
     );
