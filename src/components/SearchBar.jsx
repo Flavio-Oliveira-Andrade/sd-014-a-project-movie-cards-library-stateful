c23ebc80762a9bcaa7931e6cbe4c613e4a46a750
MartinSilva <martinempreendedor@gmail.com>
2021-09-09 10:50:40 -0300

    item 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,8 @@ class SearchBar extends Component {
       onSelectedGenreChange } = this.props;
 
     return (
-      <div />
+      <form data-testid="search-bar-form">
+      </form>
     );
   }
 }
