a15fea3230c14d32b7658756af36fe020e87e6db
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-09 18:21:59 -0300

    cfaz checagem de tipos do componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor() {
@@ -113,5 +114,9 @@ class AddMovie extends Component {
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
