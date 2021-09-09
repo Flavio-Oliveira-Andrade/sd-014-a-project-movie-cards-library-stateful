fd364d89386146342c46cbf25cdd40f12fb5ceb9
Amanda Zotelli <amzotelli@gmail.com>
2021-09-09 18:41:47 -0300

    tentando resolver erros node e etc, reiniciando os arquivos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import movies from '../data';
-
-class MovieLibrary extends React.Component {
-  render() {
-    return (
-      <h1>oi</h1>
-    );
-  }
-}
-
-export default MovieLibrary;
+// reiniciando
