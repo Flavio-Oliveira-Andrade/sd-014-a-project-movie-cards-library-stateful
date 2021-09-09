9911bcb27f61ae30edd61ba17689614af1306119
unamednada <dias.gustavo.dias@gmail.com>
2021-09-09 19:42:52 -0300

    Create addmovie and start work on movielibrary

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,103 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    const name = target.name === 'image' ? 'imagePath' : target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const { onClick } = this.props;
+    this.setState((state) => {
+      onClick(state);
+      return {
+        subtitle: '',
+        title: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre: 'action',
+      };
+    });
+  }
+
+  createInput({ label, type, name, value }) {
+    return (
+      <label data-testid={ `${name}-input-label` } htmlFor={ `${name}-input` }>
+        { label }
+        <input
+          type={ type }
+          name={ name }
+          value={ value }
+          data-testid={ `${name}-input` }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form>
+        { this.createInput({
+          label: 'Título', type: 'text', name: 'title', value: { title } }) }
+        { this.createInput({
+          label: 'Subtítulo', type: 'text', name: 'subtitle', value: { subtitle } }) }
+        { this.createInput({
+          label: 'Imagem', type: 'text', name: 'image', value: { imagePath } }) }
+        <label data-testid="storyline-input-label" htmlFor="storyline-input">
+          <textarea
+            value={ storyline }
+            data-testid="storyline-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        { this.createInput({
+          label: 'Avaliação', type: 'number', name: 'rating', value: { rating } }) }
+        <label data-testid="genre-input-label" htmlFor="genre-input">
+          <select
+            value={ genre }
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button
+          type="button"
+          data-testid="send-button"
+          onClick={ this.handleClick }
+        >
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,17 +7,28 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    const { movies } = this.props;
     this.state = {
-
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
     };
   }
 
   render() {
-    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
         <MovieList movies={ movies } />
         <AddMovie />
       </div>
