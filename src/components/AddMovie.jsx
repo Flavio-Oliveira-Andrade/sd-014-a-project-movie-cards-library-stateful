6231335c367d93c5b7ed9b95ad795c7d805e7e48
Viviane Freitas <satierfviviane@gmail.com>
2021-09-09 16:08:12 -0300

    Req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,12 +11,37 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
   }
 
   render() {
+    const {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
     const { onClick } = this.props; // callback
+
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input-label" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
