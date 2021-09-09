daf742126ea8b67e2e35f9b183d81aa372639f9d
Maurício Leite <mauricioleite1@gmail.com>
2021-09-09 15:55:55 -0300

    Requisito 06 e 07

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends Component {
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="input-subtitle">
+          Subtítulo
+          <input id="input-subtitle" type="text" value="" />
+        </label>
+        <label htmlFor="input-subtitle">
+          Título
+          <input id="input-subtitle" type="text" value="" />
+        </label>
+
+        <button type="submit" onClick={ onClick }>Adicionar filme</button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
