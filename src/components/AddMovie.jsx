6539aefbac0a316c1eb5e22d51aae0520121cecf
Guinxard <lucasmfarias3@gmail.com>
2021-09-09 16:52:27 -0300

    Requisitos 6 a 13 ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,84 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) { // gerencia as alterações dentro da SearchBar.
+    this.setState({
+      [target.name]: target.value }); // de acordo com o nome do campo selecionado, faz a alteração de seu conteúdo.
+  } // Caso seja uma checkbox, altera o atributo checked, caso não seja, altera para o valor selecionado/digitado.
+
+  inputCreator(type, name, value, dataTestid) { // cria os campos de input de acordo com as informações dos campos
+    return (
+      <input
+        type={ type }
+        name={ name }
+        value={ value }
+        onChange={ this.handleChange }
+        data-testid={ dataTestid }
+      />
+    );
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          { this.inputCreator('text', 'title', title, 'title-input') }
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          { this.inputCreator('text', 'subtitle', subtitle, 'subtitle-input') }
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          Imagem
+          { this.inputCreator('text', 'imagePath', imagePath, 'image-input') }
+        </label>
+        <label htmlFor="movie-storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            value={ storyline }
+            onChange={ this.handleChange }
+            data-testid="storyline-input"
+          />
+        </label>
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Avaliação
+          { this.inputCreator('number', 'rating', rating, 'rating-input') }
+        </label>
+        <label htmlFor="movie-genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="genre"
+            value={ genre }
+            onChange={ this.handleChange }
+            data-testid="genre-input"
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button type="submit" data-testid="send-button" onClick={ this.handleClick }>
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import movies from '../data';
+import Proptypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -7,19 +7,32 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
+      movies,
     };
     this.onSearchTextChange = this.handleChange.bind(this);
     this.onBookmarkedChange = this.handleChange.bind(this);
     this.onSelectedGenreChange = this.handleChange.bind(this);
   }
 
-  handleChange({ target }) {
+  handleChange({ target }) { // gerencia as alterações dentro da SearchBar.
     this.setState({
-      [target.name]: (target.type === checkbox ? target.checked : target.value) });
+      [target.name]: (target.type === checkbox ? target.checked : target.value) }); // de acordo com o nome do campo selecionado, faz a alteração de seu conteúdo.
+  } // Caso seja uma checkbox, altera o atributo checked, caso não seja, altera para o valor selecionado/digitado.
+
+  movieFilter = ({ searchText, selectedGenre, bookmarkedOnly, movies }) => movies
+    .filter((movie) => movie.title.includes(searchText))
+    .filter((movie) => (bookmarkedOnly ? movie.bookmarked : true))
+    .filter((movie) => (selectedGenre ? movie.genre === selectedGenre : true));
+
+  clickHandler(movie) { // gerencia o evento onClick.
+    this.setState(({ movies }) => ({ // faz a alteração do objeto movies, recebido via prop, para incrementar o array existente com o novo filme.
+      movies: [...movies, movie],
+    }));
   }
 
   render() {
@@ -32,11 +45,15 @@ class MovieLibrary extends React.Component {
           bookmarkedOnly={ bookmarkedOnly }
           selectedGenre={ selectedGenre }
         />
-        <MovieList movies={ movies } />
-        <AddMovie />
+        <MovieList movies={ this.movieFilter(this.state) } />
+        <AddMovie onClick={ this.clickHandler } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: Proptypes.arrayOf(Proptypes.object).isRequired,
+};
 
 export default MovieLibrary;
