a67b32f5dd70ddd899a26a1f32da122a1b484d29
juli0c3sar <jcmuchiutti@gmail.com>
2021-09-08 23:49:27 -0300

    adicionando func handleInput para selecionar e modificar o state title no onChange

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.handleInput = this.handleInput.bind(this);
+
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label htmlFor="titulo" data-testid="title-input-label">
+        <label htmlFor="title" data-testid="title-input-label">
           Título
           <input
             type="text"
-            name="titulo"
+            name="title"
             id=""
             data-testid="title-input"
-
+            value={ title }
+            onChange={ this.handleInput }
           />
 
         </label>
