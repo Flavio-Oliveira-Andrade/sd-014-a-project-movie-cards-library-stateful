bff46abce359995717a59ebd55212faa02cec118
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-09 15:05:12 -0300

    Rendering a tag select inside component SearchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -32,6 +32,19 @@ class SearchBar extends React.Component {
             name="bookmarkedOnly"
           />
         </label>
+        <label htmlFor data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
