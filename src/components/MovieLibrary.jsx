2e3b0f5ed044efe673a3078b0e78f8d37ec892a1
brunofeu <brunofeu89@gmail.com>
2021-09-08 21:43:40 -0300

    add filter for text select and genre

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,45 +4,51 @@ import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
-  constructor() {
+  constructor(props) {
     super();
+    const { movies } = props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      // movies: {movies},
+      movies,
     };
   }
-  
+
   handleChange = (event) => {
+    const { movies } = this.props;
     const { name, type, checked, value } = event.target;
+    const filter = movies.filter(({ title, subtitle, storyline , genre}) => title.includes(value)
+      || subtitle.includes(value) || storyline.includes(value) || genre === value);
     this.setState({
       [name]: type === 'checkbox' ? checked : value,
+      movies: filter,
     });
-  }
-  
+    if (type === 'checkbox') {
+      this.setState({
+        movies: checked ? movies.filter(({ bookmarked }) => bookmarked) : movies,
+      });
+    }
+}
+
   render() {
-    const { movies } = this.props;
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-    // console.log({movies})
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-
         <SearchBar
           searchText={ searchText }
-          onSearchTextChange={this.handleChange}
-          bookmarkedOnly={bookmarkedOnly}
-          onBookmarkedChange={this.handleChange}
-          selectedGenre={selectedGenre}
-          onSelectedGenreChange={this.handleChange}
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
         />
-        <MovieList movies={movies}/>
+        <MovieList movies={ movies } />
 
-        <AddMovie onClick={this.onClick} />
+        <AddMovie onClick={ this.onClick } />
       </div>
     );
   }
 }
 
 export default MovieLibrary;
-
