ce49d218dd525b0acae12c4dba082497c3916834
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-09 10:20:38 -0300

    Add exercise 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <section>
+        ola
+      </section>
+    );
+  }
+}
+
+export default MovieLibrary;
