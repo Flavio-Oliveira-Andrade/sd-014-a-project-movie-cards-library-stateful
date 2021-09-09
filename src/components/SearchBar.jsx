f082ad2bf9995354a03843aad054daec0e378942
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-09 14:59:33 -0300

    Rendering tag input type checkbox inside tag form with specific attributes.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,20 +5,31 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     const { searchText,
-    onSearchTextChange, 
-    bookmarkedOnly,
-    onBookmarkedChange,
-    selectedGenre,
-    onSelectedGenreChange } = this.props;
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props;
 
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor>
+        <label data-testid="text-input-label" htmlFor="searchText">
           Inclui o texto:
           <input
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
+            name="searchText"
+          />
+        </label>
+        <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            data-testid="checkbox-input"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            name="bookmarkedOnly"
           />
         </label>
       </form>
