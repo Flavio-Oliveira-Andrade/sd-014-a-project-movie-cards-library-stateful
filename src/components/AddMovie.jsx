29eab45e22f1879e95cf0202fec850401d382a1d
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-09 17:35:42 -0300

    cumpre requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -67,6 +67,20 @@ class AddMovie extends Component {
           Avaliação
           { this.customInput('number', 'rating', rating, 'rating-input') }
         </label>
+        <label htmlFor="select-genre" data-testid="genre-input-label">
+          Gênero
+          <select
+            name="select-genre"
+            id="select-genre"
+            data-testid="genre-input"
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option" selected>Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+
       </form>
     );
   }
