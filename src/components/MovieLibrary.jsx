6b5967b52e99a61eefe04c49695d448723d973ff
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-09-09 16:55:59 -0300

    Add exercise 18

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -13,14 +13,42 @@ class MovieLibrary extends React.Component {
       movies: props.movies,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.bookmarkedFilter = this.bookmarkedFilter.bind(this);
+    this.genreFilter = this.genreFilter.bind(this);
   }
 
   handleChange({ target }) {
+    if (target.type === 'checkbox') {
+      this.setState({ [target.name]: target.checked });
+      return;
+    }
     this.setState({ [target.name]: target.value });
+  }
+
+  bookmarkedFilter(movie) {
+    const { bookmarkedOnly } = this.state;
+    if (bookmarkedOnly) {
+      return movie.bookmarked;
+    }
+    return true;
+  }
+
+  genreFilter(movie) {
+    const { selectedGenre } = this.state;
+    if (selectedGenre !== '') {
+      return movie.genre === selectedGenre;
+    }
+    return true;
   }
 
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const movieBookmark = movies.filter((movie) => {
+      if (this.bookmarkedFilter(movie) && this.genreFilter(movie)) {
+        return movie.title.toLowerCase().includes(searchText.toLocaleLowerCase());
+      }
+      return false;
+    });
     return (
       <section>
         <SearchBar
@@ -31,7 +59,7 @@ class MovieLibrary extends React.Component {
           selectedGenre={ selectedGenre }
           onSelectedGenreChange={ this.handleChange }
         />
-        <MovieList movies={ movies } />
+        <MovieList movies={ movieBookmark } />
       </section>
     );
   }
