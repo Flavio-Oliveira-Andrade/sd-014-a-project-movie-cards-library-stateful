630ee34ced38a61c6fb9adb49096fef347dd3d1e
César Meira <cesarmeirasouza@gmail.com>
2021-09-09 15:28:55 -0300

    Req 3 99%

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <section>
         <form action="" data-testid="search-bar-form">
-          <input type="text" />
+          <label htmlFor="search" data-test-id="text-input-label">
+            Inclui o texto:
+            <input
+              type="text"
+              id="search"
+              value={ searchText }
+              onChange={ onSearchTextChange }
+              data-testid="text-input"
+            />
+          </label>
         </form>
       </section>
     );
   }
 }
+
+SearchBar.defaultProps = {
+  searchText: '',
+  onSearchTextChange: null,
+};
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+};
 
 export default SearchBar;
