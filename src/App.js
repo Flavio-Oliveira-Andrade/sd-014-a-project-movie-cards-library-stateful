651d00f46ba998af13465ee688d07dd962d88103
Pally <francisca.bezerr@gmail.com>
2021-09-09 15:42:53 -0300

    questao 16 e 17 ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import Header from './components/Header';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
+
 export default App;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,49 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    // <MovieLibrary /> deve receber como props:
+    const { movies } = this.props;
+    // 16 - Configure o estado inicial do componente MovieLibrary
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const valor = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: valor,
+    });
+  }
+
   render() {
-    return (<AddMovie />);
+    return (
+      <div>
+        <AddMovie />
+        <SearchBar
+          searchText={ searchText }
+          onSelectedGenreChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onBookmarkedChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onSearchTextChange={ this.handleChange }
+        />
+        <MovieList />
+      </div>
+    );
   }
 }
 
