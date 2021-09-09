14d30aca9fc25af718d1eb7a0e75adbabf6dc23e
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-08 22:32:18 -0300

    cumpre requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -36,12 +36,16 @@ class AddMovie extends Component {
 
   render() {
     const { onClick } = this.props;
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="input-text-addmovie" data-testid="title-input-label">
           Título
           { this.customInput('text', 'title', title, 'title-input') }
+        </label>
+        <label htmlFor="input-text-addmovie" data-testid="subtitle-input-label">
+          Subtítulo
+          { this.customInput('text', 'subtitle', subtitle, 'subtitle-input') }
         </label>
       </form>
     );
