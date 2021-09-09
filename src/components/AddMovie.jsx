b4e0a60bfd09898afc714cfc73fa7856d4709f3a
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-09 20:18:02 -0300

    Rendering a form tag inside component AddMovie and also one text input inside form component.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,12 +15,36 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = inicialState;
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
-      <div />
+      <form data-testid="add-movie-form">
+        <label
+          htmlFor="title"
+          data-testid="title-input-label"
+        >
+          Título
+          <input
+            type="text"
+            name="title"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
