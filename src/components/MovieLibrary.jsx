51111c8b3ad00842163393c659acc4136f494e1c
Viviane Freitas <satierfviviane@gmail.com>
2021-09-10 14:31:43 -0300

    Req 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,30 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+// import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  // constructor() {
+  //   super();
+  //   const { movies } = props;
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+  //     movies,
+  //   };
+  //   this.onSearchTextChange = this.onSearchTextChange(bind);
+  //   this.onBookmarkedChange = this.onSearchTextChange(bind);
+  //   this.onSelectedGenreChange = this.onSelectedGenreChange(bind);
+  // }
+
+  render() {
+    // const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    return (
+      <div>
+        <p>Awesome project</p>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
