4591b7a5c561eba9ded3ffdfbd160123ea76b156
Amanda Zotelli <amzotelli@gmail.com>
2021-09-10 16:51:18 -0300

    requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 
 class MovieLibrary extends React.Component {
-  render() {
-    return (
-      <h1>oi</h1>
-    );
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
   }
 }
 
-// O componente <MovieLibrary /> possui como estado as seguintes propriedades:
-
-// searchText: guarda o texto de busca por filmes;
-// bookmarkedOnly: um boolean que guarda se é para filtrar por filmes favoritados ou não;
-// selectedGenre: guarda o gênero do filme selecionado para poder fazer a filtragem;
-// movies: guarda a lista de filmes.
-// Ou seja, o estado de <MovieLibrary /> contém a lista de filmes e os filtros a serem aplicados sobre a listagem.
-
-// O estado inicial do componente <MovieLibrary /> deve ser:
-
-// searchText: '';
-// bookmarkedOnly: false;
-// selectedGenre: '';
-// movies: a lista de filmes passadas pela props movies.
+render(); {
+  return (
+    <h1>oi</h1>
+  );
+}
 
 export default MovieLibrary;
