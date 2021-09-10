3f8466a8b4fa07223eb56c82d1ccb291a6856723
Lua <luabaronto@hotmail.com>
2021-09-10 15:48:51 -0300

    Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+class FormRating extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <label data-testid="rating-input-label" htmlFor="rating">
+        Avaliação
+        <input
+          type="number"
+          name="rating"
+          id="rating"
+          value={ value }
+          onChange={ onChange }
+          data-testid="rating-input"
+        />
+      </label>
+    );
+  }
+}
+
+FormRating.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default FormRating;
