0e025b779ae2935842d39144d6c336efaccd2e2e
AlineCarolina <eualine2017@gmail.com>
2021-09-10 19:56:41 -0300

    requisito 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -7,7 +7,7 @@ class AddMovie extends React.Component {
     super();
 
     this.state = {
-      // subtitle: '',
+      subtitle: '',
       title: '',
       /* imagePath: '',
       storyline: '',
@@ -21,19 +21,29 @@ class AddMovie extends React.Component {
   handleChange(event) {
     this.setState({
       title: event.target.value,
+      subtitle: event.target.value,
     });
   }
 
   render() {
-    const { title /* subtitle, imagePath, storyline, rating, genre */ } = this.state;
+    const { title, subtitle /* imagePath, storyline, rating, genre  */ } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="subititulo">
+        <label data-testid="title-input-label" htmlFor="titulo">
           Título
           <input
             type="text"
             data-testid="title-input"
             value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="subtitle-input-label" htmlFor="subtitulo">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
             onChange={ this.handleChange }
           />
         </label>
