2cc260fe386651a54a8c177bdfa134f986323226
AlineCarolina <eualine2017@gmail.com>
2021-09-10 18:19:05 -0300

    requisito 6 e 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,33 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  /* constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  } */
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="subtitulo-input-label" htmlFor="subititulo">
+          <input />
+        </label>
+      </form>
+    );
+  }
+}
+
+/* AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}; */
+
+export default AddMovie;
