e51c65a8d5d50294d014bbbfe3ecea7984e330c5
Dayane <dayanebmartins@gmail.com>
2021-09-09 21:39:40 -0300

    componetizei a tag title em um novo arquivo ,adiciona requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,19 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
 
 class AddMovie extends React.Component {
   render() {
     return (
-      <div> </div>
+      <form data-testid="add-movie-form">
+        <Title />
+      </form>
     );
   }
 }
 
+AddMovie.propTypes = {
+  onclick: PropTypes.func.isRequired,
+};
 export default AddMovie;
