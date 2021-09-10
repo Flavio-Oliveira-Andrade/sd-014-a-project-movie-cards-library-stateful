54c9fb0d6086fed0d9da93613bb40c73cc56e020
Jonathan Ferreira <j0n4t@hotmail.com>
2021-09-10 13:38:10 -0300

    Requisitos 15 a 17! ✔

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,11 +8,16 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      movies: props.movies,
+    };
   }
 
   render() {
-    const { movies } = this.props;
+    const { movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
