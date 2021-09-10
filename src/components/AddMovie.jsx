faa03711aacbd6ccc98c98e169224b3453e78351
vanessabastos <vanessarsb83@gmail.com>
2021-09-09 21:57:39 -0300

    enviando requisito 6,7,8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form" htmlFor="form">
+        <label data-testid="title-input-label" htmlFor="title">
+          Titulo
+          <input
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleChange }
+            text="text"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
