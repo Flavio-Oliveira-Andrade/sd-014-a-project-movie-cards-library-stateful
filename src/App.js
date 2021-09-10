723397917320b02f4d1f9ceca28fe09157eb285a
jhonathancs <jhonathan_jcs@hotmail.com>
2021-09-09 20:40:15 -0500

    ultimo commit
    rever item 19 porq ainda nao ta funcionando

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+// import SearchBar from './components/SearchBar';
+// import AddMovie from './components/AddMovie';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
+      <MovieLibrary movies={ movies } />
+      {/* <SearchBar />
+      <AddMovie /> */}
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,86 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+    applyFilter = () => {
+      const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+      const { movies } = this.props;
+      const filteredMovies = movies
+        .filter(
+          (movie) => movie.title.toUpperCase().includes(searchText.toUpperCase())
+          || movie.subtitle.toUpperCase().includes(searchText.toUpperCase())
+          || movie.storyline.toUpperCase().includes(searchText.toUpperCase()),
+        )
+        .filter(
+          (movie) => bookmarkedOnly === false || movie.bookmarked === bookmarkedOnly,
+        )
+        .filter((movie) => selectedGenre === '' || movie.genre === selectedGenre);
+      this.setState({ movies: filteredMovies });
+    };
+
+  onSearchTextChange = (event) => {
+    this.setState({ searchText: event.target.value }, this.applyFilter);
+  };
+
+  onBookmarkedChange = (event) => {
+    this.setState({ bookmarkedOnly: event.target.checked }, this.applyFilter);
+  };
+
+  onSelectedGenreChange = (event) => {
+    this.setState({ selectedGenre: event.target.value }, this.applyFilter);
+  };
+
+  addMovie = (newMovie) => {
+    const { movies } = this.props;
+    this.setState({ movies: [...movies, newMovie] });
+  };
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.addMovie } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default MovieLibrary;
