6f42861d5fc1e224585ebd9029f62328ef23aa88
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-10 09:21:15 -0300

    botao de adc filme ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,8 +25,8 @@ class AddMovie extends React.Component {
   // }
 
   render() {
-    const { subtitle, imagePath, storyline, rating, genre } = this.state;
-    const { newMovie, onClick, title, change } = this.props;
+    // const {  } = this.state;
+    const { onClick, title,subtitle, rating, imagePath, storyline,  genre, change } = this.props;
     // newMovie.titulo = title
     // newMovie.subtitulo = subtitle
     // newMovie.imagem = imagePath
@@ -38,15 +38,15 @@ class AddMovie extends React.Component {
         <label htmlFor="titulo" data-testid="title-input-label">Título</label>
         <input name="title" type="text" data-testid="title-input" value={ title } onChange={ change } />
         <label htmlFor="subTitulo" data-testid="subtitle-input-label">Subtítulo</label>
-        <input name="subtitle" type="text" data-testid="subtitle-input" value={ subtitle } onChange={ this.changeText } />
+        <input name="subtitle" type="text" data-testid="subtitle-input" value={ subtitle } onChange={ change } />
         <label htmlFor="image" data-testid="image-input-label">Imagem</label>
-        <input name="imagePath" type="text" value={ imagePath } data-testid="image-input" onChange={ this.changeText } />
+        <input name="imagePath" type="text" value={ imagePath } data-testid="image-input" onChange={ change } />
         <label htmlFor="sinopse" data-testid="storyline-input-label">Sinopse</label>
-        <textarea name="storyline" id="storyline" cols="30" rows="10" data-testid="storyline-input" value={ storyline } onChange={ this.changeText } />
+        <textarea name="storyline" id="storyline" cols="30" rows="10" data-testid="storyline-input" value={ storyline } onChange={ change } />
         <label htmlFor="avaliation" data-testid="rating-input-label">Avaliação</label>
-        <input type="number" value={ rating } data-testid="rating-input" name="rating" onChange={ this.changeText } />
+        <input type="number" value={(rating)} data-testid="rating-input" name="rating" onChange={ change } />
         <label htmlFor="genero" data-testid="genre-input-label">Gênero</label>
-        <select name="genre" id="genre" onChange={ this.changeText } value={ genre } data-testid="genre-input">
+        <select name="genre" id="genre" onChange={ change } value={ genre } data-testid="genre-input">
           <option value="action" data-testid="genre-option">Ação</option>
           <option value="comedy" data-testid="genre-option">Comédia</option>
           <option value="thriller" data-testid="genre-option">Suspense</option>
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
+import lista from '../data'
 
 class MovieLibrary extends Component {
   constructor() {
@@ -10,8 +11,13 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      mov: {titulo: '', subtitulo: '', imagem: '', sinopse: '', nota: 0, genero: ''},
-      title: ''
+      mov: { titulo: '', subtitulo: '', imagem: '', sinopse: '', nota: 0, genero: '' },
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
     this.clicked = this.clicked.bind(this);
     this.searching = this.searching.bind(this);
@@ -23,9 +29,25 @@ class MovieLibrary extends Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  clicked(e) {
+  clicked (e) {
     e.preventDefault();
-    console.log(AddMovie);
+    lista.push({
+      title: this.state.title,
+      subtitle: this.state.subtitle,
+      storyline: this.state.storyline,
+      rating: parseFloat(this.state.rating),
+      imagePath: this.state.imagePath,
+      genre: this.state.genre,
+    })
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    })
+    // console.log(this.state)
   }
 
   searching(e) {
@@ -45,7 +67,7 @@ class MovieLibrary extends Component {
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre, mov, title} = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, mov, title, subtitle, imagePath, storyline, rating, genre } = this.state;
     const { movies } = this.props;
     const titleFilter = movies.filter((cur) => cur.title.includes(searchText) === true || cur.subtitle.includes(searchText) === true || cur.storyline.includes(searchText) === true);
     let filterFavorit;
@@ -62,10 +84,10 @@ class MovieLibrary extends Component {
     }
     return (
       <>
-        <SearchBar searchText={ searchText } onSearchTextChange={ this.searching } bookmarkedOnly={ bookmarkedOnly } onBookmarkedChange={ this.bookmarking } selectedGenre={ selectedGenre } onSelectedGenreChange={ this.genre } />
-        <MovieList movies={ filterGenre } />
-        <AddMovie onClick={ this.clicked } newMovie={ mov } title={title} change={this.changeText}/>
-        {console.log(this.state.title)}
+        <SearchBar searchText={searchText} onSearchTextChange={this.searching} bookmarkedOnly={bookmarkedOnly} onBookmarkedChange={this.bookmarking} selectedGenre={selectedGenre} onSelectedGenreChange={this.genre} />
+        <MovieList movies={filterGenre} />
+        <AddMovie onClick={this.clicked} newMovie={mov} title={title} subtitle={subtitle} imagePath={imagePath} storyline={storyline} rating={rating} genre={genre} change={this.changeText} />
+        {/* {console.log(this.state)} */}
       </>
     );
   }
