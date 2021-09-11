bad8b227ea04ac2ed9bdb2d79cb233675bfa2bb3
Marcio <marciofonseca.ufrj@gmail.com>
2021-09-10 21:00:02 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import movies from './data';
+import SearchBar from './components/SearchBar';
 
-function App() {
+class App extends React.Component {
+
+
+
+  render() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies = { movies }/>
+      <SearchBar/>
     </div>
   );
 }
+}
+
+
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchText: '',
+
+    }
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <form 
+      data-testid="search-bar-form">
+      formulário
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
+
+
