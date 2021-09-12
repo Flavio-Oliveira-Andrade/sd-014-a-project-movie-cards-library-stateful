a52e39536df3989c9d2ac8f81b10f1bffcf3ee80
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-12 15:09:17 -0300

    Changes some HTML tags to maki more semantic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Header />
-      <h1>Start</h1>
-    </div>
+      <MovieList movies={ movies } />
+    </main>
   );
 }
 
