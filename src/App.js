db90471f5e8c622552f2434cc369dc26f5801f1a
gsdu-arte <gabi15duarte@hotmail.com>
2021-09-12 17:29:29 -0300

    Requisitos 6 e 7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
+import movies from './data';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={ movies } />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,33 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <div>
+        <form data-testid="add-movie-form"></form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
