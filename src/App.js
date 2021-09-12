b061ad7710bf75eab3a23d499d361875e966617a
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-09-12 22:31:39 +0100

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
+import movies from './data';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Header />
-    </div>
+      <MovieLibrary movies={ movies } />
+    </main>
   );
 }
 
