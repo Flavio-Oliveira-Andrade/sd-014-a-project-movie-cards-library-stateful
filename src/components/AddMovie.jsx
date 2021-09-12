e1a9001dfb872b4f98e2c4d780cccd194c7f29a7
vanessabastos <vanessarsb83@gmail.com>
2021-09-12 09:10:08 -0300

    enviando requistos 10 e 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,46 +1,17 @@
 import React from 'react';
+import Title from './Title';
+import Subtitle from './Subtitle';
+import ImagePath from './ImagePath';
+import Textarea from './Textarea';
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: '',
-      subtitle: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
   render() {
-    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title">
-          Título
-          <input
-            value={ title }
-            data-testid="title-input"
-            onChange={ this.handleChange }
-            text="text"
-            id="title"
-            name="title"
-          />
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle">
-          Subtítulo
-          <input
-            value={ subtitle }
-            data-testid="subtitle-input"
-            onChange={ this.handleChange }
-            id="subtitle"
-            text="text"
-            name="subtitle"
-          />
-        </label>
+        <Title />
+        <Subtitle />
+        <ImagePath />
+        <Textarea />
       </form>
     );
   }
