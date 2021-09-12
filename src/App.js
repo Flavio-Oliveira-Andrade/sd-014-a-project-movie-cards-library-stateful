fcc511e9cf787c687ca451f2af38683d6b3e6115
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-09-12 00:24:32 -0300

    15 Requisito => pulei o 3 passo do requisito 14 para fazer o resto e depois voltar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import Header from './components/Header';
 import './App.css';
 
 // Meus Imports:
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Requisito 01 ao 05 */}
-      <SearchBar />
-      {/* Requisito 06 ao 14 */}
-      <AddMovie />
+      {/* Requisito 15 */}
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,27 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Meus Imports:
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <div>
+        {/* Requisito 01 ao 05 */}
+        <SearchBar />
+        {/* Requisito 06 ao 14 */}
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
