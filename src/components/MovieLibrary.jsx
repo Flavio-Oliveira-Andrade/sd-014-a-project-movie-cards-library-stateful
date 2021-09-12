5c6cfde53156775b81c3494c755e6b3b13b086c6
gsdu-arte <gabi15duarte@hotmail.com>
2021-09-12 20:27:42 -0300

    Requisito 16

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <div>
-
+        <SearchBar />
+        <MovieList movies={ movies } />
       </div>
     );
   }
