ae9bb4c394d17bb308bee03d714ed27545c2809d
dandara-dias <dandaradias.contato@gmail.com>
2021-09-12 03:19:45 -0300

    Reqs 6 a 10 (erro no lint)

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,83 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { title, subtitle, imagePath } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="titulo" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            value={ title }
+            id="titulo"
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="subtitulo" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            id="subtitulo"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="imagem" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="imagem"
+            value={ imagePath }
+            data-testid="image-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+};
+
+AddMovie.defaultProps = {
+  onClick: null,
+};
+
+export default AddMovie;
