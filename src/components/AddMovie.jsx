2c81a1c77d6fd4d2f40c553541ef6251f614202e
vanessadcds <vanessadacosta.des@gmail.com>
2021-09-13 09:20:15 -0300

    tentando resolver AddMovie requisito 8 e 9, codigo quebrando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,22 +4,43 @@ class AddMovie extends React.Component {
   constructor() {
     super();
     this.state = {
-      subtitle:'',
-      title:'',
-      imagePath:'',
-      storyline:'',
-      rating:0,
-      genre:'action',
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handelchange(event) {
+    const { name } = event.target;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const { onclick } = this.props;
+    const { title } = this.state;
     return (
-      <div>
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Titulo
+          <input
+            data-testid="title-input"
+            type="text"
+            name={ title }
+            value={ title }
+            onChange={ this.handelchange }
+          />
+        </label>
         onclick=
         { onclick }
-      </div>
+      </form>
     );
   }
 }
