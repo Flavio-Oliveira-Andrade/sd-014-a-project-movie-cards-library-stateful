c5b70efd3350961a3be307c084e94d39f2a0be9b
Flavio Pires <flavioasp77@gmail.com>
2021-09-13 19:46:47 -0300

    Requisito 13 select criado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -67,6 +67,19 @@ class AddMovie extends React.Component {
         <label htmlFor="rating" data-testid="rating-input-label">
           { this.inputConstructor('number', 'rating', 'rating-input', rating) }
         </label>
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            data-testid="genre-input"
+            name="genre"
+            value={ genre }
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
