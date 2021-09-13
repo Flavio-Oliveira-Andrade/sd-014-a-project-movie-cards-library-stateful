71fdf6fb38dd44d97e16f5062021a1c517f6337e
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-09-13 12:33:22 -0300

    Renderiza um input dentro do formulario em SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,14 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-
+        <label data-testid="text-input-label">
+          Inclui o texto:
+        </label>
+        <input
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
       </form>
     );
   }
