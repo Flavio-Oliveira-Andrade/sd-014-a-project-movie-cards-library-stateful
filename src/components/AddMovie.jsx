52c08abc3a3a290b08c22b2babb31dab495c6557
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-13 08:30:52 -0300

    AddMovie sem erro no lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PutInput from './PutInput';
 import SelectInput from './SelectInput';
+import inputs from './inputs';
 
 class AddMovie extends React.Component {
   render() {
@@ -14,48 +15,29 @@ class AddMovie extends React.Component {
       storyline,
       genre,
       change } = this.props;
-    const inputs = [
-      { data: 'title',
-        name: 'title',
-        type: 'text',
-        label: 'Título',
-        value: title },
-      { data: 'subtitle',
-        name: 'subtitle',
-        type: 'text',
-        label: 'Subtítulo',
-        value: subtitle },
-      { data: 'image',
-        name: 'imagePath',
-        type: 'text',
-        label: 'Imagem',
-        value: imagePath },
-      { data: 'storyline',
-        name: 'storyline',
-        type: 'textarea',
-        label: 'Sinopse',
-        value: storyline },
-      { data: 'rating',
-        name: 'rating',
-        type: 'number',
-        label: 'Avaliação',
-        value: rating }];
-
-    return (<form data-testid="add-movie-form">
-      <fieldset>
-        {inputs.map((input) => (<PutInput
-          key={ input.name }
-          data={ input.data }
-          name={ input.name }
-          type={ input.type }
-          label={ input.label }
-          value={ input.value }
-          onChange={ change }
-        />))}
-        <SelectInput data-testid="genre-input" change={ change } genre={ genre } />
-        <button type="submit" data-testid="send-button" onClick={ onClick }>Adicionar filme</button>
-      </fieldset>
-    </form>);
+    const values = [title, subtitle, imagePath, storyline, rating];
+    return (
+      <form data-testid="add-movie-form">
+        <fieldset>
+          {inputs.map((input, i) => (<PutInput
+            key={ input.name }
+            data={ input.data }
+            name={ input.name }
+            type={ input.type }
+            label={ input.label }
+            value={ values[i] }
+            onChange={ change }
+          />))}
+          <SelectInput data-testid="genre-input" change={ change } genre={ genre } />
+          <button
+            type="submit"
+            data-testid="send-button"
+            onClick={ onClick }
+          >
+            Adicionar filme
+          </button>
+        </fieldset>
+      </form>);
   }
 }
 
---
+++ src/components/inputs.js
@@ -0,0 +1,28 @@
+const inputs = [{
+  data: 'title',
+  name: 'title',
+  type: 'text',
+  label: 'Título',
+}, {
+  data: 'subtitle',
+  name: 'subtitle',
+  type: 'text',
+  label: 'Subtítulo',
+}, {
+  data: 'image',
+  name: 'imagePath',
+  type: 'text',
+  label: 'Imagem',
+}, {
+  data: 'storyline',
+  name: 'storyline',
+  type: 'textarea',
+  label: 'Sinopse',
+}, {
+  data: 'rating',
+  name: 'rating',
+  type: 'number',
+  label: 'Avaliação',
+}];
+
+export default inputs;
