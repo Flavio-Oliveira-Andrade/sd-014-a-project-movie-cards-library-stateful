de492c7ba31ce67cff70b18dd7712a44311cfacc
vanessadcds <vanessadacosta.des@gmail.com>
2021-09-12 21:18:48 -0300

    Requisitos 1,2 e 5 passando no lit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class SearchBar extends React.Component {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
-      onBookmarketChange,
+      onBookmarkedChange,
       selectedGenre,
       onSelectedGenreChange,
     } = this.props;
@@ -17,10 +18,36 @@ class SearchBar extends React.Component {
           Inclui o texto
           <input
             data-testid="text-input"
+            type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
             name="searchText"
           />
+        </label>
+
+        <label data-testid="checkbox-input-label" htmlFor="booblmarkedOnly">
+          Mostrar somente favoritos
+          <input
+            data-testid="text-input"
+            type="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange }
+            name="bookmarkedOnlny"
+          />
+        </label>
+        <label data-testid="select-input-label" htmlFor="selectedGenre">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            name="selectedGenre"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
@@ -31,7 +58,7 @@ SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarketChange: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
   selectedGenre: PropTypes.string.isRequired,
   onSelectedGenreChange: PropTypes.func.isRequired,
 };
