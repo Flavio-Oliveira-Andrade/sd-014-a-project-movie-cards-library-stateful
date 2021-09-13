fe77f35ea901742b9953da9aafec97b001b5b993
vanessadcds <vanessadacosta.des@gmail.com>
2021-09-13 07:13:14 -0300

    Requisito 6 que nao passa ainda

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,27 @@
-// implement AddMovie componfgient here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle:'',
+      title:'',
+      imagePath:'',
+      storyline:'',
+      rating:0,
+      genre:'action',
+    };
+  }
+
+  render() {
+    const { onclick } = this.props;
+    return (
+      <div>
+        onclick=
+        { onclick }
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
