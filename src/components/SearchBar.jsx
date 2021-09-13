68209985cab1e06cf430d9e7d2663acd30563cca
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-09-13 15:14:34 -0300

    Requisito 01 - SearchBar adicionado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form action=""></form>
+    )
+  }
+}
