aa33abe226f1bce5cc75869f0ed880ca561881be
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-09-13 16:14:54 -0300

    Requisito 06 - Adicionado o componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,9 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovieComponent extends Component {
+
+}
+
+export default AddMovie;
