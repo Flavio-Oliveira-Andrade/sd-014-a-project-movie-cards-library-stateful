c5400b073e9dfa63c8c1d36852a8816255690548
Yasmin <yasminleticia_@hotmail.com>
2021-09-12 22:00:36 -0300

    requisito 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,39 +3,51 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
   constructor() {
     super();
+    this.changeState = this.changeState.bind(this);
     this.state = {
-      // subtitle: '',
+      subtitle: '',
       title: '',
       /* imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action', */
+            storyline: '',
+            rating: 0,
+            genre: 'action', */
     };
   }
 
-  changeTitle({ target }) {
-    const { name } = target;
+  changeState({ target }) {
+    const { name, value } = target;
     this.setState({
-      [name]: name.value,
+      [name]: value,
     });
   }
 
   render() {
     // const { onclick } = this.props;
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
 
       <form action="" data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
+        <label htmlFor="titleInput" data-testid="title-input-label">
           Título
           <input
             type="text"
-            name="titleInput"
+            name="title"
             value={ title }
-            onChange={ this.changeTitle }
+            onChange={ this.changeState }
             data-testid="title-input"
           />
 
+        </label>
+
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.changeState }
+          />
         </label>
       </form>
 
