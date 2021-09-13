d9ca83899e18a6853ff3205f8c9aa742055d0f11
vixemari <marianarocha308@gmail.com>
2021-09-12 22:07:11 -0300

    Requesito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,18 @@ import React from 'react';
 class AddMovie extends React.Component {
   render() {
     return (
-      <div />
+      <div>
+        <form data-testid="add-movie-form">
+          <label data-testid="title-input-label" htmlFor>
+            <input
+              type="text"
+              value="title"
+              data-testid="title-input"
+            />
+            Título
+          </label>
+        </form>
+      </div>
 
     );
   }
