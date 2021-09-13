cb94f5242a43380ab3e36f774626c3d5a68d217d
Flavio Pires <flavioasp77@gmail.com>
2021-09-13 18:43:08 -0300

    Requisitos 9, 10 e 11 finalizados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -24,8 +24,40 @@ class AddMovie extends React.Component {
           Título
           <input
             type="text"
+            data-testid="title-input"
             name="title"
             value={ title }
+            onChange={ title }
+          />
+        </label>
+        <label htmlFor="title" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            data-testid="subtitle-input"
+            name="subtitle"
+            value={ subtitle }
+            onChange={ subtitle }
+          />
+        </label>
+        <label htmlFor="imagePath" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+            name="imagePath"
+            value={ imagePath }
+            onChange={ imagePath }
+          />
+        </label>
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          Imagem
+          <textarea
+            type="storyline"
+            data-testid="storyline-input"
+            name="storyline"
+            value={ storyline }
+            onChange={ storyline }
           />
         </label>
       </form>
