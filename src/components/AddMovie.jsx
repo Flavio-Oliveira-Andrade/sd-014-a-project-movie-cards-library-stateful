aa4ac5f5b1967a7d59b1bed8dcd1916cdc6e08bc
jmporto <jmporto@outlook.com>
2021-09-13 04:41:46 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       title: '',
       subtitle: '',
@@ -15,11 +15,32 @@ class AddMovie extends React.Component {
     };
   }
 
+  handleChange = ({ target }) => this.setState({
+    [target.name]: (target.type === 'checkbox' ? target.checked : target.value),
+  });
+
+  input(name, type, value, dataTestId) {
+    return (
+      <input
+        name={ name }
+        type={ type }
+        value={ value }
+        data-testid={ dataTestId }
+        onChange={ this.handleChange }
+      />
+    );
+  }
+
   render() {
     // desestrutura o estado inicial de "this.state"
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          { this.input('title', 'text', title, 'title-input') }
+        </label>
+      </form>
     );
   }
 }
