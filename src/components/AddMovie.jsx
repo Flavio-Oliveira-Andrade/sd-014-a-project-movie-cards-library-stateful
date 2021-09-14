1fa03e1b7217a7c52c114e2cc9e0b147a660e006
DanFilipe2000 <anielilipe90@gmail.com>
2021-09-13 22:42:38 -0300

    Mais alguns requisitos /2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,14 @@
 // implement AddMovie component here
 import React, { Component } from 'react';
 
+import Title from './FormComponents/Title';
+import Subtitle from './FormComponents/Subtitle';
+import ImagePath from './FormComponents/ImagePath';
+import Storyline from './FormComponents/Storyline';
+import Rating from './FormComponents/Rating';
+import Genre from './FormComponents/Genre';
+import Button from './FormComponents/Button';
+
 class AddMovie extends Component {
   constructor() {
     super();
@@ -13,14 +21,9 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
-
-    this.handdleClick = this.handdleClick.bind(this);
-    this.makeInput = this.makeInput.bind(this);
-    this.haddleChange = this.haddleChange.bind(this);
   }
 
-  handdleClick(event) {
-    event.preventDefault();
+  handdleClick = () => {
     // const { onClick } = this.props;
     this.setState({
       title: '',
@@ -32,91 +35,21 @@ class AddMovie extends Component {
     });
   }
 
-  haddleChange(event) {
+  haddleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   }
-
-  makeOptions(valor, text) {
-    return (
-      <>
-        <option
-          className="inputs"
-          value={ valor[0] }
-          data-testid="genre-option"
-        >
-          { text[0] }
-        </option>
-        <option
-          className="inputs"
-          value={ valor[1] }
-          data-testid="genre-option"
-        >
-          { text[1] }
-        </option>
-        <option
-          className="inputs"
-          value={ valor[2] }
-          data-testid="genre-option"
-        >
-          { text[2] }
-        </option>
-      </>
-    );
-  }
-
-  makeInput(type, idtest, valor, nome) {
-    return (
-      <input
-        className="inputs"
-        type={ type }
-        data-testeid={ idtest }
-        defaultValue={ valor }
-        name={ nome }
-        onChange={ this.haddleChange }
-      />
-    );
-  } // Ideia da função para incluir inputs foi discutida com o meu colega de turma Thiago Oshiro.
 
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="inputs">
-          Título
-          { this.makeInput('text', 'title-input', title, 'title') }
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="inputs">
-          Subtítulo
-          { this.makeInput('text', 'subtitle-input', subtitle, 'subtitle') }
-        </label>
-        <label data-testid="image-input-label" htmlFor="inputs">
-          Imagem
-          { this.makeInput('text', 'image-input', imagePath, 'imagePath') }
-        </label>
-        <label data-testid="storyline-input-label" htmlFor="inputs">
-          Sinopse
-          { this.makeInput('text-area', 'storyline-input', storyline, 'storyline')}
-        </label>
-        <label data-testid="rating-input-label" htmlFor="inputs">
-          Avaliação
-          { this.makeInput('number', 'rating-input', rating, 'rating') }
-        </label>
-        <label data-testid="genre-input-label" htmlFor="inputs">
-          Gênero
-          <select
-            name="genre"
-            data-testid="genre-input"
-            defaultValue={ genre }
-            onChange={ this.haddleChange }
-          >
-            { this.makeOptions(
-              ['action', 'comedy', 'thriller'], ['Ação', 'Comédia', 'Suspense'],
-            )}
-          </select>
-        </label>
-        <button type="submit" data-testid="send-button" onClick="">
-          Adicionar filme
-        </button>
+        <Title title={ title } haddleChange={ this.handdleChange } />
+        <Subtitle subtitle={ subtitle } haddleChange={ this.handdleChange } />
+        <ImagePath imagePath={ imagePath } haddleChange={ this.handdleChange } />
+        <Storyline storyline={ storyline } haddleChange={ this.handdleChange } />
+        <Rating rating={ rating } haddleChange={ this.handdleChange } />
+        <Genre genre={ genre } haddleChange={ this.handdleChange } />
+        <Button onClick={ this.handdleClick } />
       </form>
     );
   }
