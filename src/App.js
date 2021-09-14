f10e34138392f66bdd83a8c82a0a7934d026dad8
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-09-13 22:46:54 -0300

    requisitos 16 e 17

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      inputText: '',
-      inputCheckboxFavorites: false,
-      selectGenre: '',
-    };
-  }
-
-  handleChange({ target }) { // desestruturação do event para não precisar escrever event.target
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value; // necessário para conseguir pegar o valor do checkbox, se for checkbox pega o checked e se não for pega o value
-    this.setState({ [name]: value }); // interpolando o valor da variável para ser o nome da chave do objeto
-  }
-
-  handleClick() {
-
-  }
-
   render() {
-    const { inputText, inputCheckboxFavorites, selectGenre } = this.state;
     return (
       <div className="App">
         <Header />
-        <SearchBar
-          searchText={ inputText }
-          onSearchTextChange={ this.handleChange }
-          bookmarkedOnly={ inputCheckboxFavorites }
-          onBookmarkedChange={ this.handleChange }
-          selectedGenre={ selectGenre }
-          onSelectedGenreChange={ this.handleChange }
-        />
         <MovieLibrary movies={ movies } />
-        <AddMovie onClick={ this.handleClick } />
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      inputText: '',
+      inputCheckboxFavorites: false,
+      selectGenre: '',
+    };
+  }
+
+  handleChange({ target }) { // desestruturação do event para não precisar escrever event.target
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value; // necessário para conseguir pegar o valor do checkbox, se for checkbox pega o checked e se não for pega o value
+    this.setState({ [name]: value }); // interpolando o valor da variável para ser o nome da chave do objeto
+  }
+
+  handleClick() {
+
+  }
+
   render() {
+    const { inputText, inputCheckboxFavorites, selectGenre } = this.state;
+    const { movies } = this.props;
     return (
-      <p>Movie Library</p>
+      <main>
+        <SearchBar
+          searchText={ inputText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ inputCheckboxFavorites }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.handleClick } />
+      </main>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
