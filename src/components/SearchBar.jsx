d8e4725de8210faea091bc1f6fb763b510a077a2
Saulo Lima <saulolima.c@protonmail.com>
2021-09-14 15:32:32 -0300

    Requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="text-input">
+        <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
           <input
             type="text"
