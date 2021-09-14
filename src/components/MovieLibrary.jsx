da2b1efd33e8af80e21aae923b647707b9dff9bc
Flavio Pires <flavioasp77@gmail.com>
2021-09-14 15:09:57 -0300

    Criada o filtro de pesquisa de filmes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+
+    this.movieFilter = this.movieFilter.bind(this);
+  }
+
+  movieFilter() {
+
+  }
+
   render() {
-    const { movie } = this.props;
+
+    const { searchText, selectedGenre, bookmarkedOnly } = this.state;
+
     return (
       <section>
-        teste
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList />
       </section>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieLibrary;
