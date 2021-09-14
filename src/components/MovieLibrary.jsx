13ce539c7d01f2338a4aa205243c60f8fdc88b7b
wendeldemelo <demelowendel@gmail.com>
2021-09-13 23:41:54 -0400

    projeto iniciado

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,26 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  // constructor(props) {
+  //  super(props);
+  // }
+
+  render() {
+    // const { searchText } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar /* searchText={ searchText } */ />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */ }
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
