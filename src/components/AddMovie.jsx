1e7466612bf20e599f58f0e183c80c4fcc6ea297
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-09-14 15:39:46 -0300

    Requisito 06 - Parte 2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,8 +2,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class AddMovieComponent extends Component {
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  return (
 
+  )
 }
 
 export default AddMovie;
