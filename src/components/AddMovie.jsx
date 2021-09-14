3eac0c29cc41aeba1abbd54d956515b0f83abb35
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-09-14 19:10:32 -0300

    Requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,24 +4,40 @@ import SubTitle from './SubTitle';
 import ImagePath from './ImagePath';
 import StoryLine from './StoryLine';
 import RatingForm from './RatingForm';
+import Genre from './Genre';
 
 class AddMovie extends Component {
   constructor() {
     super();
 
     this.state = {
-      // genre: 'action',
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <Title />
-        <SubTitle />
-        <ImagePath />
-        <StoryLine />
-        <RatingForm />
+        <Title value={ title } onChange={ this.handleChange } />
+        <SubTitle value={ subtitle } onChange={ this.handleChange } />
+        <ImagePath value={ imagePath } onChange={ this.handleChange } />
+        <StoryLine value={ storyline } onChange={ this.handleChange } />
+        <RatingForm value={ rating } onChange={ this.handleChange } />
+        <Genre value={ genre } onChange={ this.handleChange } />
       </form>
     );
   }
