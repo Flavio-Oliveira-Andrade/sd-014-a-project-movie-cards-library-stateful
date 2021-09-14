9683f05822991fc2a19e7d4dff75853a69d231b0
Victor Hugo Souza <victoraster2014@gmail.com>
2021-09-14 13:25:37 -0300

    correções do Search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,13 +8,13 @@ class SearchBar extends Component {
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="input-text" data-testid="text-input-label">
+        <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
           <input
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
-            data-testid="input-text"
+            data-testid="text-input"
           />
         </label>
         <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
@@ -24,18 +24,21 @@ class SearchBar extends Component {
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
           />
-          Mostrar somenete favoritos
+          Mostrar somente favoritos
         </label>
-        <select
-          data-testid="select-input"
-          value={ selectedGenre }
-          onChange={ onSelectedGenreChange }
-        >
-          <option data-test-id="select-option" value="">Todos</option>
-          <option data-test-id="select-option" value="action">Ação</option>
-          <option data-test-id="select-option" value="comedy">Comédia</option>
-          <option data-test-id="select-option" value="thriller">Suspense</option>
-        </select>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
