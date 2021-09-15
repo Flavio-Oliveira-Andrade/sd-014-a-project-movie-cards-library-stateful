7626ece4820d7bdca82d68f5b5ce3c14b94625bd
AquilesDantas <aquilesdant@gmail.com>
2021-09-14 23:12:51 -0300

    Requisito 15 ao 17

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,81 +2,135 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class AddMovie extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyline: '',
-  //     rating: 0,
-  //     genre: 'action',
-  //   };
-  // }
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick(event) {
+    const { onClick } = this.props;
+    onClick(event);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
 
   renderTitle() {
+    const { title } = this.state;
     return (
       <label data-testid="title-input-label" htmlFor="title-input">
         Título
         <input
           data-testid="title-input"
           type="text"
+          name="title"
+          value={ title }
+          onChange={ this.handleChange }
         />
       </label>
     );
   }
 
   renderSubtitle() {
+    const { subtitle } = this.state;
     return (
       <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
         Subtítulo
         <input
           data-testid="subtitle-input"
           type="text"
+          name="subtitle"
+          value={ subtitle }
+          onChange={ this.handleChange }
         />
       </label>
     );
   }
 
   renderImage() {
+    const { imagePath } = this.state;
     return (
       <label data-testid="image-input-label" htmlFor="image-input">
         Imagem
-        <input data-testid="image-input" type="text" />
+        <input
+          data-testid="image-input"
+          type="text"
+          name="imagePath"
+          value={ imagePath }
+          onChange={ this.handleChange }
+        />
       </label>
     );
   }
 
   renderStoryline() {
+    const { storyline } = this.state;
     return (
       <label data-testid="storyline-input-label" htmlFor="storyline-input">
         Sinopse
         <input
           data-testid="storyline-input"
           type="text"
+          name="storyline"
+          value={ storyline }
+          onChange={ this.handleChange }
         />
       </label>
     );
   }
 
   renderRating() {
+    const { rating } = this.state;
     return (
       <label data-testid="rating-input-label" htmlFor="rating-input">
         Avaliação
         <input
           data-testid="rating-input"
           type="number"
-          defaultValue={ 0 }
+          name="rating"
+          value={ rating }
+          onChange={ this.handleChange }
         />
       </label>
     );
   }
 
   renderGenre() {
+    const { genre } = this.state;
     return (
       <label data-testid="genre-input-label" htmlFor="genre-input">
         Gênero
-        <select data-testid="genre-input">
+        <select
+          data-testid="genre-input"
+          name="genre"
+          value={ genre }
+          onChange={ this.handleChange }
+        >
           <option data-testid="genre-option" value="action">Ação</option>
           <option data-testid="genre-option" value="comedy">Comédia</option>
           <option data-testid="genre-option" value="thriller">Suspense</option>
@@ -86,7 +140,6 @@ export class AddMovie extends Component {
   }
 
   render() {
-    const { onClick } = this.props;
     return (
       <div>
         <form data-testid="add-movie-form">
@@ -98,7 +151,7 @@ export class AddMovie extends Component {
           {this.renderGenre()}
           <button
             type="submit"
-            onClick={ onClick }
+            onClick={ this.handleClick }
             data-testid="send-button"
           >
             Adicionar filme
---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+export class MovieLibrary extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+
+  //   };
+  // }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+
+  }).isRequired,
+};
+
+export default MovieLibrary;
