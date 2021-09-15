91e53f48ff84123d51d8a8c9d115edc5aeadf5bf
Victor Veloso <victorveloso13@gmail.com>
2021-09-15 16:34:10 -0300

    Cria componente AddMovie e começa a renderizar um form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,28 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="" data-testid="title-input-label">
+          <input type="text" name="" id="" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
