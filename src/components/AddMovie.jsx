6acfcce06dd8e6daa5c8bec8c00043bd97de0add
Danilo Menezes <danilomenezes66@gmail.com>
2021-09-14 21:18:16 -0300

    Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  // https://pt-br.reactjs.org/docs/react-component.html#constructor
   constructor() {
     super();
     this.state = {
@@ -12,15 +13,30 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+    // referencia do bind: https://stackoverflow.com/questions/32317154/react-uncaught-typeerror-cannot-read-property-setstate-of-undefined?rq=1
+  }
+
+  handleChange(event) {
+    this.setState({ title: event.target.value });
   }
 
   render() {
     const { onClick } = this.props;
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+
     return (
       <section>
         <form data-testid="add-movie-form">
-
+          <label htmlFor="title-input" data-testid="title-input-label">
+            Título
+            <input
+              type="text"
+              data-testid="title-input"
+              value={ title }
+              onChange={ this.handleChange }
+            />
+          </label>
         </form>
       </section>
     );
