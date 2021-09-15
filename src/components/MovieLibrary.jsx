0d9c4353453f6cb112708ca1b1333787ea2eb55a
delpapajoao <joaodelpapa1@gmail.com>
2021-09-15 18:40:15 -0300

    Requisito 1, 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,48 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends Reac.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="searchText">
+          Search:
+          <input
+            type="text"
+            id="searchText"
+            name="searchText"
+          />
+        </label>
+
+        <label htmlFor="bookMark">
+          Book:
+          <input
+            type="text"
+            id="bookMark"
+            name="bookMarkOnly"
+          />
+        </label>
+
+        <label htmlFor="select">
+          Select Genre:
+          <input
+            type="select"
+            id="select"
+            name="selectGenre"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default MovieLibrary;
