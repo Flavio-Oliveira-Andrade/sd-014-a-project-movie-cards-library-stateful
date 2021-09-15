ae962847015d1177b6594cd64c51a67d2237bd49
Eduardo Rocha <eduardorocha-dev@protonmail.ch>
2021-09-14 23:43:58 -0300

    Refatora AddMovie para atender todos os testes que dependem apenas deste componente

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -34,6 +34,65 @@ class AddMovie extends React.Component {
     );
   }
 
+  generateImageTag(valueParameter) {
+    return (
+      <label htmlFor="imagePath" data-testid="image-input-label">
+        Imagem
+        <input
+          type="text"
+          value={ valueParameter }
+          data-testid="image-input"
+          onChange={ this.handleChange }
+          name="imagePath"
+        />
+      </label>
+    );
+  }
+
+  generateStorylineTag(valueParameter) {
+    return (
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          data-testid="storyline-input"
+          name="storyline"
+          value={ valueParameter }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  generateSelectTag(valueParameter) {
+    return (
+      <label data-testid="genre-input-label" htmlFor="genre">
+        Gênero
+        <select
+          data-testid="genre-input"
+          name="genre"
+          onChange={ this.handleChange }
+          value={ valueParameter }
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  generateButton(callback) {
+    return (
+      <button
+        data-testid="send-button"
+        onClick={ callback }
+        type="button"
+      >
+        Adicionar filme
+      </button>
+    );
+  }
+
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     const { onClick } = this.props;
@@ -41,41 +100,11 @@ class AddMovie extends React.Component {
       <form data-testid="add-movie-form">
         { this.generateInputTag('Título', 'text', 'title', title) }
         { this.generateInputTag('Subtítulo', 'text', 'subtitle', subtitle) }
-        { this.generateInputTag('Imagem', 'text', 'image', imagePath) }
-
-        <label htmlFor="rating" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            data-testid="storyline-input"
-            name="rating"
-            value={ storyline }
-            onChange={ this.handleChange }
-          />
-        </label>
-
+        { this.generateImageTag(imagePath) }
         { this.generateInputTag('Avaliação', 'number', 'rating', rating) }
-
-        <label data-testid="genre-input-label" htmlFor="genre">
-          Gênero
-          <select
-            data-testid="genre-input"
-            name="genre"
-            onChange={ this.handleChange }
-            value={ genre }
-          >
-            <option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
-          </select>
-        </label>
-
-        <button
-          data-testid="send-button"
-          onClick={ onClick }
-          type="button"
-        >
-          Adicionar filme
-        </button>
+        { this.generateStorylineTag(storyline) }
+        { this.generateSelectTag(genre) }
+        { this.generateButton(onClick) }
       </form>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    const { movies } =
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        <SearchBar />
+        <SearchBar searchText={ searchText } />
+        <MovieList movies={ this.props.movies } />
         <AddMovie />
       </div>
     );
