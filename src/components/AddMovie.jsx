2f15469ecf1051f20ff6e2709e76dbac08f1198e
Luiz Seidl <luizseidl@gmail.com>
2021-09-15 11:30:29 -0300

    tentando fazer evaluator identificar button e rating

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,9 +4,9 @@ import AddMovieTitle from './AddMovieTitle';
 import AddMovieSubtitle from './AddMovieSubtitle';
 import AddMovieImage from './AddMovieImage';
 import AddMovieStoryline from './AddMovieStoryline';
-import AddMovieRating from './AddMovieRating';
+// import AddMovieRating from './AddMovieRating';
 import AddMovieGenre from './AddMovieGenre';
-import AddMovieBtn from './AddMovieBtn';
+// import AddMovieBtn from './AddMovieBtn';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -54,9 +54,27 @@ class AddMovie extends React.Component {
         <AddMovieSubtitle value={ subtitle } onChange={ this.handleChange } />
         <AddMovieImage value={ imagePath } onChange={ this.handleChange } />
         <AddMovieStoryline value={ storyline } onChange={ this.handleChange } />
-        <AddMovieRating value={ rating } onChange={ this.handleChange } />
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            id="rating-input"
+            value={ rating }
+            // O input deve ter seu valor inicial provido pelo estado inicial do componente, via rating;
+            data-testid="rating-input"
+            onChange={ this.handleChange }
+            // A propriedade onChange deve atualizar o estado de <AddMovie />, atribuindo a rating a avaliação atual continda no input.
+          />
+        </label>
         <AddMovieGenre value={ genre } onChange={ this.handleChange } />
-        <AddMovieBtn onClick={ this.handleClick } />
+        <button
+          type="submit"
+          data-testid="send-button"
+          onClick={ this.handleClick }
+          // A propriedade onClick do botão invoca uma função definida por você, em <AddMovie />, que: Executa a callback passada para o componente <AddMovie /> via props, chamada onClick, que recebe como parâmetro o estado atual de <AddMovie />; Reseta o estado de <AddMovie />, voltando para o inicial, conforme mencionado anteriormente.
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
