da285307f7ae3baa4943e9cff36096f7689172b4
vixemari <marianarocha308@gmail.com>
2021-09-14 22:16:05 -0300

    Requesito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,10 @@
 // implement MovieLibrary component here
 import React from 'react';
-import AddMovie from './AddMovie';
-import MovieList from './MovieList';
-import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   render() {
     return (
-      <div>
-        <SearchBar />
-        <MovieList />
-        <AddMovie />
-      </div>
+      <div />
 
     );
   }
