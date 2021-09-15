882c8f6178c226e8012b089e458e116bbb037e98
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-09-15 16:28:50 -0300

    requisito 15 resolvido

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>{}</div>
+    );
+  }
+}
+
+export default MovieLibrary;
