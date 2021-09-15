31669fb59adbda11a9839e63c1650939a05a94aa
Matheus Pozett <maa_souza11@hotmail.com>
2021-09-15 17:31:52 -0300

    Requisitos 16, 17 e 19

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = { movies };
+  }
+
+  AddMovie = (movie) => {
+    this.setState((state) => ({ movies: [...state.movies, movie] }));
+  }
+
   render() {
+    const { movies } = this.state;
     return (
-      <div>
-        Test
-      </div>
+      <section>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.AddMovie } />
+      </section>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
 
 export default MovieLibrary;
