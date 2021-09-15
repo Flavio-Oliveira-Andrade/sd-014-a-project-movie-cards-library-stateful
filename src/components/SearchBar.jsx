43bd181291bce1959a6c7d73dea45be26dd912d2
Lucas Silveira <lucgs88@gmail.com>
2021-09-15 20:02:46 -0300

    fixing item 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,7 +17,7 @@ class SearchBar extends Component {
         <label htmlFor="searchText" data-testid="text-input-label">
           Inclui o texto
           <input
-            type="text-area"
+            type="text"
             data-testid="text-input"
             value={ searchText }
             onChange={ onSearchTextChange }
