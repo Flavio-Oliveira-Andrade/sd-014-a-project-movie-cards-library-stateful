eaa450f796a874a943baa750775fe8b604d9a61f
Fernando F Monteiro <fernandoflmbatera@gmail.com>
2021-09-15 15:32:03 -0300

    Requisito 16 - initialState em MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,18 @@
 // implement MovieLibrary component here
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
-
-// }
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    }
+  }
+}
