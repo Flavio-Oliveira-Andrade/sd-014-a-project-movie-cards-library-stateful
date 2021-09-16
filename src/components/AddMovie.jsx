eeb387023148ae7ba00340ea312bf8c7221fcca6
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-09-16 09:44:15 -0300

    Req "8 - Renderize um input do tipo texto dentro do formulário em `<AddMovie />` Pass.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,31 +5,49 @@ class AddMovie extends React.Component {
     super();
     this.state = {
       // subtitle: '',
-      // title: '',
+      title: '',
       // imagePath: '',
       // storyline: '',
       // rating: 0,
       // genre: 'action',
     };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ title: event.target.value });
   }
 
   render() {
     // const { onClick } = this.props;
-    // const {
+    const {
     //   subtitle,
-    //   title,
+      title,
     //   imagePath,
     //   storyline,
     //   rating,
     //   genre
-    // } = this.state;
+    } = this.state;
 
     return (
       <div>
         <form
           data-testid="add-movie-form"
           action=""
-        />
+        >
+          <label
+            data-testid="title-input-label"
+            htmlFor="231"
+          >
+            Título
+            <input
+              type="text"
+              value={ title }
+              data-testid="title-input"
+              onChange={ this.onChange }
+            />
+          </label>
+        </form>
       </div>
 
     );
