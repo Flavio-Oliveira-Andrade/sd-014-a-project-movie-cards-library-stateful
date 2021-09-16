61af21b4e5e4add2117e49dcaa689f2b991b02b7
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-09-16 09:02:57 -0300

    Req "6 e 7 Renderize um formulário dentro de `<AddMovie />`" Pass.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      // title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
   render() {
+    // const { onClick } = this.props;
+    // const {
+    //   subtitle,
+    //   title,
+    //   imagePath,
+    //   storyline,
+    //   rating,
+    //   genre
+    // } = this.state;
+
     return (
-      <div>AddMovie</div>
+      <div>
+        <form
+          data-testid="add-movie-form"
+          action=""
+        />
+      </div>
+
     );
   }
 }
