67d26ab9a8a0d3ab6ade288494a39726cf9c260c
Carla <carladmaximo@gmail.com>
2021-09-16 06:54:45 -0300

    commit pra verificar a porcentagem do projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,8 +11,6 @@ class AddMovie extends React.Component {
       title: '',
       imagePath: '',
       storyline: '',
-      rating: '',
-      genre: 'action',
     };
   }
 
@@ -24,37 +22,37 @@ class AddMovie extends React.Component {
   }
 
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { subtitle, title, imagePath, storyline } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title">
           Título
           <input
-            type="text"
             id="title"
+            type="text"
             value={ title }
-            data-testid="title-input"
             onChange={ this.handleChange }
+            data-testid="title-input"
           />
         </label>
         <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
           <input
-            type="text"
             id="subtitle"
+            type="text"
             value={ subtitle }
-            data-testid="subtitle-input"
             onChange={ this.handleChange }
+            data-testid="subtitle-input"
           />
         </label>
         <label data-testid="image-input-label" htmlFor="imagePath">
           Imagem
           <input
-            type="text"
             id="imagePath"
+            type="text"
             value={ imagePath }
-            data-testid="image-input"
             onChange={ this.handleChange }
+            data-testid="image-input"
           />
         </label>
         <label data-testid="storyline-input-label" htmlFor="storyline">
