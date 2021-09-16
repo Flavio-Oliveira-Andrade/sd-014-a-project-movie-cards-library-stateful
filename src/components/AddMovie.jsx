a073c0d6834c517e34ef0c27b8570cc553681d0e
Eduardo Rocha <eduardorocha-dev@protonmail.ch>
2021-09-15 22:06:06 -0300

    Refatora AddMovie para atender todos os casos de teste

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Title from './addMovieComponents/Title';
+import Subtitle from './addMovieComponents/Subtitle';
+import Image from './addMovieComponents/Image';
+import Storyline from './addMovieComponents/Storyline';
+import NewMovieRating from './addMovieComponents/NewMovieRating';
+import Genre from './addMovieComponents/Genre';
+
 class AddMovie extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -19,92 +27,36 @@ class AddMovie extends React.Component {
     [target.name]: (target.type === 'checkbox' ? target.checked : target.value),
   });
 
-  generateInputTag(labelText, type, data, value) {
-    return (
-      <label htmlFor={ data } data-testid={ `${data}-input-label` }>
-        { labelText }
-        <input
-          type={ type }
-          value={ value }
-          data-testid={ `${data}-input` }
-          onChange={ this.handleChange }
-          name={ data }
-        />
-      </label>
-    );
-  }
-
-  generateImageTag(valueParameter) {
-    return (
-      <label htmlFor="imagePath" data-testid="image-input-label">
-        Imagem
-        <input
-          type="text"
-          value={ valueParameter }
-          data-testid="image-input"
-          onChange={ this.handleChange }
-          name="imagePath"
-        />
-      </label>
-    );
-  }
-
-  generateStorylineTag(valueParameter) {
-    return (
-      <label htmlFor="storyline" data-testid="storyline-input-label">
-        Sinopse
-        <textarea
-          data-testid="storyline-input"
-          name="storyline"
-          value={ valueParameter }
-          onChange={ this.handleChange }
-        />
-      </label>
-    );
-  }
-
-  generateSelectTag(valueParameter) {
-    return (
-      <label data-testid="genre-input-label" htmlFor="genre">
-        Gênero
-        <select
-          data-testid="genre-input"
-          name="genre"
-          onChange={ this.handleChange }
-          value={ valueParameter }
-        >
-          <option data-testid="genre-option" value="action">Ação</option>
-          <option data-testid="genre-option" value="comedy">Comédia</option>
-          <option data-testid="genre-option" value="thriller">Suspense</option>
-        </select>
-      </label>
-    );
-  }
-
-  generateButton(callback) {
-    return (
-      <button
-        data-testid="send-button"
-        onClick={ callback }
-        type="button"
-      >
-        Adicionar filme
-      </button>
-    );
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
-    const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        { this.generateInputTag('Título', 'text', 'title', title) }
-        { this.generateInputTag('Subtítulo', 'text', 'subtitle', subtitle) }
-        { this.generateImageTag(imagePath) }
-        { this.generateInputTag('Avaliação', 'number', 'rating', rating) }
-        { this.generateStorylineTag(storyline) }
-        { this.generateSelectTag(genre) }
-        { this.generateButton(onClick) }
+        <Title value={ title } onChange={ this.handleChange } />
+        <Subtitle value={ subtitle } onChange={ this.handleChange } />
+        <Image value={ imagePath } onChange={ this.handleChange } />
+        <Storyline value={ storyline } onChange={ this.handleChange } />
+        <NewMovieRating value={ rating } onChange={ this.handleChange } />
+        <Genre value={ genre } onChange={ this.handleChange } />
+        <button
+          type="button"
+          data-testid="send-button"
+          onClick={ this.handleClick }
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
