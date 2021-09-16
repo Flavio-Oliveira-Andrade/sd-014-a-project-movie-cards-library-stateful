6c05396faa468cff371ccd60b6ef868aa474d884
Yan Brasileiro <yan.brasileiro@gmail.com>
2021-09-16 12:07:49 -0300

    Requisito 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -65,6 +65,19 @@ class AddMovie extends React.Component {
           Avaliação
           { this.createInput('number', 'rating', rating, 'rating-input') }
         </label>
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            data-testid="genre-input"
+            name="genre"
+            onChange={ this.handleChange }
+            value={ genre }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
