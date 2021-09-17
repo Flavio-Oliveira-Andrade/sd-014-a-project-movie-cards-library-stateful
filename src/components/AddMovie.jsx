d7dfd31ab0385a3d67dff3f5a0fc22a18c2d499e
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-09-17 02:22:22 -0300

    Requisitos 08 - correção temporária para passar no lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,12 +4,12 @@ class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: '',
+      // subtitle: '',
       title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
   }
