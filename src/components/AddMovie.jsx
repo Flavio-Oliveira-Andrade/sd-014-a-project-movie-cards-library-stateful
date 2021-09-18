f93bd3f3a471ce410c782b24a53390593cd976ee
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-09-18 20:37:14 -0300

    Cria um componente chamado AddMovie e cria um estado inicial para ele

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,52 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath:'',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const {
+      onClick,
+    } = this.props;
+
+    return (
+      <form>
+        <label htmlFor="subtitle">
+          Subtítulo
+          <input type="text" id="subtitle" />
+        </label>
+        <label htmlFor="title">
+          Título
+          <input type="text" id="title" />
+        </label>
+        <label htmlFor="imagePath">
+          Caminho da imagem
+          <input type="text" id="imagePath" />
+        </label>
+        <label htmlFor="storyline">
+          Sinopse
+          <input type="text" id="storyline" />
+        </label>
+        <label htmlFor="rating">
+          Avaliação
+          <input type="number" id="rating" />
+        </label>
+        <label htmlFor="genre">
+          Gênero
+          <input type="text" id="genre" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
