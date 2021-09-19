e12d6fed711ac89074f65545d1c54be8f6d769c0
vanessadcds <vanessadacosta.des@gmail.com>
2021-09-19 20:15:03 -0300

    Requisitos do 10 a 14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import movies from './data';
 import AddMovie from './components/AddMovie';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar onClick={ movies } />
       <AddMovie />
     </div>
   );
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,9 +5,6 @@ class AddMovie extends React.Component {
   constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
-    // this.handleClick = this.handleClick.bind(this);
-
     this.state = {
       subtitle: '',
       title: '',
@@ -16,10 +13,12 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange({ target }) { // event.target
-    const { name } = target;
+    const { name } = target; // target.value.name
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
     this.setState({
@@ -27,11 +26,29 @@ class AddMovie extends React.Component {
     });
   }
 
+  handleSubmit(e) { // e = event
+    e.preventDefault(); // evita processamento automático
+    const { onClick } = this.props;
+    onClick(this.state);
+
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     const { title,
       subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre,
     } = this.state;
-    // const { onClick } = this.props;
 
     return (
       <form data-testid="add-movie-form">
@@ -56,9 +73,62 @@ class AddMovie extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
+
+        <label data-testid="image-input-label" htmlFor="imagePath">
+          Imagem
+          <input
+            data-testeid="image-input"
+            type="text"
+            name="imagePath"
+            value={ imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="storyline" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            name="storyline"
+            data-testid="storyline-input"
+            value={ storyline }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="rating" data-testid="rating-input-label">
+          Avaliação
+          <input
+            name="rating"
+            type="number"
+            data-testid="rating-input"
+            value={ rating }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label data-testid="genre-input-label" htmlFor="genre">
+          Gênero
+          <select
+            data-testid=""
+            name="genre"
+            value={ genre }
+            onChange={ this.handleChange }
+          >
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+        <button type="submit" data-testid="send-button" onClick={ this.handleSubmit }>
+          Adicionar filme
+        </button>
       </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
