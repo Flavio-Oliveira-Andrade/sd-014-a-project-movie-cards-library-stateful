d29a87d1ad8b081087bc7200d7a5047091b7cb1a
Lucas Sodre <lucasmeinen@yahoo.com.br>
2021-09-20 12:51:27 -0300

    requisitos 6 e 7 criando estados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,43 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  changeTitle({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: name.value,
+    });
+  }
+
+  render() {
+    // const { onclick } = this.props;
+    const { title } = this.state;
+    return (
+      <form action="" data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="titleInput"
+            value={ title }
+            onChange={ this.changeTitle }
+            data-testid="title-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
