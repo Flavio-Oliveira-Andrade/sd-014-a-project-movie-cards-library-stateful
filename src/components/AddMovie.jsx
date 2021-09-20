b44c6132fdc505234dcc5403b3ca983fc4ded012
Lucas Sodre <lucasmeinen@yahoo.com.br>
2021-09-20 13:09:21 -0300

    requisitos 8 e 9

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,7 @@ class AddMovie extends Component {
   constructor() {
     super();
     this.state = {
-      // subtitle: '',
+      subtitle: '',
       title: '',
       // imagePath: '',
       // storyline: '',
@@ -13,26 +13,36 @@ class AddMovie extends Component {
     };
   }
 
-  changeTitle({ target }) {
-    const { name } = target;
+  changeState({ target }) {
+    const { name, value } = target;
     this.setState({
-      [name]: name.value,
+      [name]: value,
     });
   }
 
   render() {
     // const { onclick } = this.props;
-    const { title } = this.state;
+    const { title, subtitle } = this.state;
     return (
       <form action="" data-testid="add-movie-form">
-        <label htmlFor="title-input" data-testid="title-input-label">
+        <label htmlFor="titleInput" data-testid="title-input-label">
           Título
           <input
             type="text"
-            name="titleInput"
+            name="title"
             value={ title }
-            onChange={ this.changeTitle }
+            onChange={ this.changeState }
             data-testid="title-input"
+          />
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            value={ subtitle }
+            data-testid="subtitle-input"
+            onChange={ this.changeState }
           />
         </label>
       </form>
