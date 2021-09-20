59c12e41bed19ead31697313de2abe22f6b028bc
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-09-20 00:47:00 -0300

    Adiciona PropTypes e refatora codigo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -29,8 +29,64 @@ class AddMovie extends React.Component {
     this.setState(this.initialState);
   }
 
+  renderGenre() {
+    const { genre } = this.state;
+    return (
+      <label htmlFor="genre" data-testid="genre-input-label">
+        Gênero
+        <select
+          name="genre"
+          id="genre"
+          value={ genre }
+          data-testid="genre-input"
+          onChange={ this.handleChange }
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderRating() {
+    const { rating } = this.state;
+
+    return (
+      <label htmlFor="rating" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          id="rating"
+          value={ rating }
+          data-testid="rating-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderStoryline() {
+    const { storyline } = this.state;
+
+    return (
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          name="storyline"
+          id="storyline"
+          cols="30"
+          rows="10"
+          value={ storyline }
+          data-testid="storyline-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { title, subtitle, imagePath } = this.state;
 
     return (
       <form data-testid="add-movie-form">
@@ -64,46 +120,10 @@ class AddMovie extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea
-            name="storyline"
-            id="storyline" 
-            cols="30"
-            rows="10"
-            value={ storyline }
-            data-testid="storyline-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input
-            type="number"
-            id="rating"
-            value={ rating }
-            data-testid="rating-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="genre" data-testid="genre-input-label">
-          Gênero
-          <select
-            name="genre"
-            id="genre"
-            value={ genre }
-            data-testid="genre-input"
-            onChange={ this.handleChange }
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
-        <button
-          data-testid="send-button"
-          onClick={ this.handleClick }
-        >
+        { this.renderStoryline() }
+        { this.renderRating() }
+        { this.renderGenre() }
+        <button type="button" data-testid="send-button" onClick={ this.handleClick }>
           Adicionar filme
         </button>
       </form>
