cdfe529d8c29a2a41dc9b33b5c3e1a6854b4d965
guiroos <guilherme.roosr@gmail.com>
2021-09-26 23:31:15 -0300

    Finalizado requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,12 +11,27 @@ export default class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { id, value } }) {
+    this.setState({ [id]: value });
   }
 
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        sim
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            id="title"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
