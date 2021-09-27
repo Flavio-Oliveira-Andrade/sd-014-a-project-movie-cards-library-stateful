b5864717d794b996d7830c078fad9b4f398e283d
Rafael M Friederick <rafael.mf.documents@gmail.com>
2021-09-27 16:40:52 -0300

    Requisitos 6 a 14 Refatorados

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,6 +6,7 @@ import AddMovieSubtitle from './AddMovieSubtitle';
 import AddMovieImage from './AddMovieImage';
 import AddMovieStoryline from './AddMovieStoryline';
 import AddMovieRating from './AddMovieRating';
+import AddMovieGenre from './AddMovieGenre';
 
 class AddMovie extends React.Component {
   constructor(props) {
@@ -19,36 +20,12 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.saveState = this.saveState.bind(this);
-    this.handleChanceTitle = this.handleChanceTitle.bind(this);
-    this.handleChanceSubtitle = this.handleChanceSubtitle.bind(this);
-    this.handleChanceImagePath = this.handleChanceImagePath.bind(this);
-    this.handleChanceStoryline = this.handleChanceStoryline.bind(this);
-    this.handleChanceRating = this.handleChanceRating.bind(this);
-    this.handleChanceGenre = this.handleChanceGenre.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChanceTitle(event) {
-    this.saveState({ key: 'title', value: (event.target.value) });
-  }
-
-  handleChanceSubtitle(event) {
-    this.saveState({ key: 'subtitle', value: (event.target.value) });
-  }
-
-  handleChanceImagePath(event) {
-    this.saveState({ key: 'imagePath', value: (event.target.value) });
-  }
-
-  handleChanceStoryline(event) {
-    this.saveState({ key: 'storyline', value: (event.target.value) });
-  }
-
-  handleChanceRating(event) {
-    this.saveState({ key: 'rating', value: (event.target.value) });
-  }
-
-  handleChanceGenre(event) {
-    this.saveState({ key: 'genre', value: (event.target.value) });
+  handleChange({ target }) {
+    const { name } = target;
+    this.saveState({ key: name, value: (target.value) });
   }
 
   saveState(content) {
@@ -66,39 +43,35 @@ class AddMovie extends React.Component {
         <form data-testid="add-movie-form">
           <AddMovieTitle
             title={ title }
-            handleChanceTitle={ this.handleChanceTitle }
+            handleChanceTitle={ this.handleChange }
           />
           <AddMovieSubtitle
             subtitle={ subtitle }
-            handleChanceSubtitle={ this.handleChanceSubtitle }
+            handleChanceSubtitle={ this.handleChange }
           />
           <AddMovieImage
             imagePath={ imagePath }
-            handleChanceImagePath={ this.handleChanceImagePath }
+            handleChanceImagePath={ this.handleChange }
           />
           <AddMovieStoryline
             storyline={ storyline }
-            handleChanceStoryline={ this.handleChanceStoryline }
+            handleChanceStoryline={ this.handleChange }
           />
           <AddMovieRating
             rating={ rating }
-            handleChanceRating={ this.handleChanceRating }
+            handleChanceRating={ this.handleChange }
           />
-          <label htmlFor="genre-input" data-testid="genre-input-label">
-            Gênero
-            <select
-              name="genre-input"
-              id="genre-input"
-              data-testid="genre-input"
-              value={ genre }
-              onChange={ this.handleChanceGenre }
-            >
-              <option value="action" data-testid="genre-option">Ação</option>
-              <option value="comedy" data-testid="genre-option">Comédia</option>
-              <option value="thriller" data-testid="genre-option">Suspense</option>
-            </select>
-          </label>
-          <button type="button" data-testid="send-button" onClick={ onClick }>Adicionar filme</button>
+          <AddMovieGenre
+            genre={ genre }
+            handleChanceGenre={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="send-button"
+            onClick={ onClick }
+          >
+            Adicionar filme
+          </button>
         </form>
       </div>
     );
