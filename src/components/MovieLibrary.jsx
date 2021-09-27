7a9ce2e711861b2970dae0cc4b581a579f796173
Rafael M Friederick <rafael.mf.documents@gmail.com>
2021-09-27 18:31:18 -0300

    Requisito 17, 18 e 19 em andamento

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,71 @@
 // implement MovieLibrary component here
 import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: (movies),
+    };
+    this.saveState = this.saveState.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+  }
+
+  onSearchTextChange({ target }) {
+    this.saveState({ searchText: target.value });
+  }
+
+  onBookmarkedChange({ target }) {
+
+  }
+
+  onSelectedGenreChange({ target }) {
+    this.saveState({ selectedGenre: target.value });
+  }
+
+  saveState(content) {
+    this.setState(content);
+  }
+
   render() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
-      <div></div>
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+        <MovieList
+          movies={ movies }
+        />
+        <AddMovie
+          onClick={ this.handleClick }
+        />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
 
 export default MovieLibrary;
