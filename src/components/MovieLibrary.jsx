7677939d54ddbecff706a625d189e7735dd28df0
Victor Veloso <victorveloso13@gmail.com>
2021-09-27 18:09:01 -0300

    Requisito 16 - MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,6 +5,12 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
   }
 
   render() {
