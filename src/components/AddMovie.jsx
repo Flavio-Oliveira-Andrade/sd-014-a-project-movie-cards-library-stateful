dc3af2691bfcc222d91b431f95e30990c8197ed3
Arthores <arthurolis41@gmail.com>
2021-09-27 20:06:19 -0300

    Desestruturaçao dos inputs

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       subtitle: '',
       title: '',
       imagePath: '',
       storyline: '',
       rating: 0,
       genre: 'action',
-   }
+    };
 
     this.handleChange = this.handleChange.bind(this);
-}
+    this.inputGenerator = this.inputGenerator.bind(this);
+  }
 
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
-     });
+    });
   }
 
   handleAdd = (event) => {
-    const { onClick } = this.props;
+    const { onClick } = this.props; // props validation
     event.preventDefault();
-    onClick( this.state );
+    onClick(this.state);
     this.setState({
       subtitle: '',
       title: '',
@@ -36,79 +36,68 @@ class AddMovie extends React.Component {
     });
   }
 
+  inputGenerator(data, type, name, value) {
+    return (
+      <input 
+        data-testid={ data }
+        type={ type }
+        name={ name }
+        value={ value }
+        onChange={ this.handleChange }      
+      />
+    )
+  }
+
+  createTextArea(data, name, value) {
+    return (
+      <textarea
+        data-testid={ data }
+        name={ name }
+        value={ value }
+        cols="25"
+        rows="1"
+        onChange={ this.handleChange }
+      />
+    );
+  }
+
   render() {
-    const { 
-      subtitle,
-      title,
-      imagePath,
-      storyline,
-      rating,
-      genre,
-    } = this.state;
-    return(
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
       <div>
         <form data-testid="add-movie-form">
           <label htmlFor="title" data-testid="title-input-label">
-            Título
-            <input
-              name="title"
-              data-testid="title-input"      
-              type="text"
-              value={ title }
-              onChange={ this.handleChange }/>
+            Titulo
+            { this.inputGenerator('title-input', 'text', 'title', title) }
           </label >
           <label htmlFor="subtitle" data-testid="subtitle-input-label">
             Subtítulo
-            <input
-              name="subtitle"
-              data-testid="subtitle-input"
-              type="text"
-              value={ subtitle }
-              onChange={ this.handleChange }/>
+            { this.inputGenerator('subtitle-input', 'text', 'subtitle', subtitle) }
           </label>
           <label htmlFor="imagePath" data-testid="image-input-label">
             Imagem
-            <input
-              name="imagePath"
-              data-testid="image-input"
-              type="text"
-              value={ imagePath }
-              onChange={ this.handleChange } />
+            { this.inputGenerator('image-input', 'text', 'image', imagePath) }
           </label>
           <label htmlFor="storyline" data-testid="storyline-input-label">
             Sinopse
-            <textarea
-              name="storyline"
-              data-testid="storyline-input"
-              cols="25" 
-              rows="1"
-              value={ storyline }
-              onChange={ this.handleChange }/>
+            { this.createTextArea('storyline-input', 'storyline', storyline) }
           </label>
           <label htmlFor="rating" data-testid="rating-input-label">
             Avaliação
-            <input
-            name="rating" 
-            data-testid="rating-input"
-            type="number"
-            value={ rating }
-            onChange={ this.handleChange }/>
+            { this.inputGenerator('rating-input', 'text', 'rating', rating) }
           </label>
           <label htmlFor="genre" data-testid="genre-input-label">
             Gênero
             <select
-            data-testid="genre-input"
-            name="genre"
-            value={ genre }
-            onChange={ this.handleChange }>
-              <option data-testid="genre-option" value="action">Ação</option>
-              <option data-testid="genre-option" value="comedy">Comédia</option>
-              <option data-testid="genre-option" value="thriller">Suspense</option>
+              data-testid="genre-input"
+              name="genre"
+              value={ genre } onChange={ this.handleChange }>
+                <option data-testid="genre-option" value="action">Ação</option>
+                <option data-testid="genre-option" value="comedy">Comédia</option>
+                <option data-testid="genre-option" value="thriller">Suspense</option>
             </select>
           </label>
-          <button
-            data-testid="send-button"
-            onClick={ this.handleAdd }>
+          <button data-testid="send-button"onClick={ this.handleAdd }>
             Adicionar filme
           </button>
         </form>
