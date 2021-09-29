d62f1c08e76814f1ef2a0648c61d60b96daf0998
Lucas Oliveira <luaks.skt8@gmail.com>
2021-09-29 11:31:27 -0300

    REQ 15 - 19: componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,59 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  handleChange = ({ target }) => this.setState({
+    [target.name]: (target.type === 'checkbox' ? target.checked : target.value),
+  });
+
+  handleClick = (movie) => {
+    this.setState(({ movies }) => ({
+      movies: [...movies, movie],
+    }));
+  }
+
+  moviesFilter = ({ searchText, selectedGenre, bookmarkedOnly, movies }) => movies
+    .filter((movie) => (movie.title.includes(searchText)
+      || movie.subtitle.includes(searchText) || movie.storyline.includes(searchText)))
+    .filter((movie) => (bookmarkedOnly ? movie.bookmarked : true))
+    .filter((movie) => (selectedGenre ? movie.genre === selectedGenre : true));
+
+  render() {
+    const { searchText, selectedGenre, bookmarkedOnly } = this.state;
+    return (
+      <main>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ this.moviesFilter(this.state) } />
+        <AddMovie onClick={ this.handleClick } />
+      </main>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
