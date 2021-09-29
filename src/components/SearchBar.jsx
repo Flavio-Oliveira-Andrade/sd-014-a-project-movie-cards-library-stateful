3e55938600a801bb678fdeacfd4fce357530206c
PeDD1 <pedrohr0909@gmail.com>
2021-09-29 17:21:09 -0300

    Added Select for Movie Genres

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,32 +11,41 @@ class SearchBar extends React.Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenre
+      onSelectedGenreChange,
     } = this.props;
 
     return (
       <form data-testid="search-bar-form" className="search-bar-form">
-        <label htmlFor="search-bar-text" data-testid="text-input-label">
+        <label htmlFor="searchText" data-testid="text-input-label">
           Inclui o Texto:
           <input
             type="text"
-            name="searchText"
-            id="search-bar-text"
             value={ searchText }
             onChange={ onSearchTextChange }
             data-testid="text-input"
           />
         </label>
-        <label htmlFor="search-bar-bookmarked" data-testid="checkbox-input-label">
+        <label htmlFor="checkBoxFav" data-testid="checkbox-input-label">
           <input
             type="checkbox"
-            name="bookmarkedOnly"
-            id="search-bar-bookmarked"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
             data-testid="checkbox-input"
           />
           Mostrar somente favoritos
+        </label>
+        <label htmlFor="selectedGenre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
