8578451da6ed2e4f28139d0076570c97a1c80abe
PeDD1 <pedrohr0909@gmail.com>
2021-09-29 17:38:49 -0300

    Rendering Form element to AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,23 @@
 import React from 'react';
 
 class AddMovie extends React.Componet {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
     return (
+      <form className="add-movie-form" data-testid="add-movie-form">
 
+      </form>
     );
   }
 }
